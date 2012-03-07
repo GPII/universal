4241cad7a2a5b5b96cf13928037d0d59832fb64a
Kasper Markus <kasper2@markus.dk>
2012-03-06 21:48:44 -0700

    Created the flowManager and launchHandler components. Unit tests still needs to be written, etc

---
+++ deviceReporter/src/deviceReporter.js
@@ -0,0 +1,39 @@
+/*!
+Device Reporter
+
+Copyright 2012 Raising the Floor - International
+
+Licensed under the New BSD license. You may not use this file except in
+compliance with this License.
+
+You may obtain a copy of the License at
+https://github.com/gpii/universal/LICENSE.txt
+*/
+
+(function () {
+	var os = require("os");
+    "use strict";
+
+    var express = require("express"),
+        fluid = require("infusion"),
+        path = require("path"),
+        gpii = fluid.registerNamespace("gpii");
+    
+    fluid.defaults("gpii.deviceReporter", {
+        gradeNames: ["fluid.littleComponent", "autoInit"],
+		preInitFunction: "gpii.deviceReporter.preInit"
+    
+    });
+    
+    gpii.deviceReporter.preInit = function (that) {
+		that.get = function (directModel, callback) {
+			callback({
+				OS: { 
+					id: os.platform(), 
+					version: os.release() 
+				}
+			});
+		}
+	}
+})();
+    
