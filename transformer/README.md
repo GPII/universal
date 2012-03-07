94a379a77ce1e7d01a1e6e39bfa9783c711207e7
Colin Clark <colinbdclark@gmail.com>
2012-03-06 17:57:05 -0700

    Adds jqUnit-based test harness and preliminary failing tests for the Settings Transformer.

---
+++ transformer/README.md
@@ -0,0 +1,7 @@
+GPII Personalization Framework: Settings Transformer
+====================================================
+
+The Settings Transformer takes:
+ # a user preferences profile 
+ # a Solution-specific transformation map 
+and produces the appropriate settings for the Solution.
---
+++ transformer/test/js/TransformerTests.js
@@ -0,0 +1,24 @@
+/*!
+GPII Settings Transformer Tests
+
+Copyright 2012 OCAD University
+
+Licensed under the New BSD license. You may not use this file except in
+compliance with this License.
+
+You may obtain a copy of the License at
+https://github.com/gpii/universal/LICENSE.txt
+*/
+
+(function () {
+    fluid.registerNamespace("gpii.tests.transformer");
+    
+    gpii.tests.transformer.runTests = function () {
+        var testCase = jqUnit.TestCase("Settings Transformer");
+
+        testCase.test("gpii.transformer.transformSettings()", function () {    
+            ok(false);
+        });
+    };
+
+}());
