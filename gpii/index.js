6c261ede8f5bce7cd6b4ec488fe3676d542c50c7
Yura Zenevich <yura.zenevich@gmail.com>
2012-03-23 12:32:57 -0400

    Added package files for flow manager and univeral itself. Removed gpii.flowManager() call from FlowManager.js it will now be called by the plafrom specific script

---
+++ gpii/index.js
@@ -0,0 +1,7 @@
+var fluid = require("infusion");
+
+fluid.require("gpiiFramework", require);
+fluid.require("matchMaker", require);
+fluid.require("deviceReporter", require);
+fluid.require("configurationManager", require);
+fluid.require("flowManager", require);
