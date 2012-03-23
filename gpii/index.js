71b803e99acb5a028255a6d2f50028bb6129d08d
Yura Zenevich <yura.zenevich@gmail.com>
2012-03-23 16:39:39 -0400

    Integrating match maker with transformer. Removing transformer data source from flow manager

--- gpii/index.js
+++ gpii/index.js
@@ -2,6 +2,7 @@ var fluid = require("infusion");
 
 fluid.require("gpiiFramework", require);
 fluid.require("matchMaker", require);
+fluid.require("transformer", require);
 fluid.require("deviceReporter", require);
 fluid.require("configurationManager", require);
 fluid.require("flowManager", require);
