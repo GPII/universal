9ff259c35f8f92722e605cb9b4e45986148bbe23
Kasper Markus <kasper2@markus.dk>
2012-04-10 11:45:42 +0200

    Committing to move changes to unix env. and test

--- tests/tests.js
+++ tests/tests.js
@@ -14,17 +14,22 @@ https://github.com/gpii/universal/LICENSE.txt
     var fluid = require("universal");
     var gpii = fluid.registerNamespace("gpii");
     var http = require("http");
+    //will hold reference to flowManager for later use
     var flowManager;    
     gpii.integrationTesting = fluid.registerNamespace("gpii.integrationTesting");
 
     gpii.integrationTesting.mockLaunchHandler = function(data) {
         fluid.log("MOCK LAUNCHER HERE");
+        //assert that the mock settingsHandler is being called at all
+        ok(true, "gpii.integrationTesting.mockLaunchHandler called");
         return data;
     };
 
     gpii.integrationTesting.mockSettingsHandler = function(data) {
         fluid.log("MOCK SETTINGS HANDLER RECIEVED: "+JSON.stringify(data));
+        //assert that the mock settingsHandler is being called at all
         ok(true, "gpii.integrationTesting.mockSettingsHandler called");
+        //create payload to return - with oldValue, newValue objects for values
         var cpy = fluid.copy(data);
         //enter array with settingsHandlers
         cpy = fluid.transform(cpy, function(settingsHandler) {
@@ -39,32 +44,43 @@ https://github.com/gpii/universal/LICENSE.txt
         return cpy;
     };
 
+    var printRec(flowManager) {
+        fluid.each(flowManager, function(v, k) {
+            console.log(k+": "+v);
+        });
+    };  
+
+    //launches the flowmanager and initializes the flowManager variable.
     gpii.integrationTesting.launchServer = function() {
         flowManager = gpii.flowManager();
     };
 
-    asyncTest("Does it launch?", function () {
+    asyncTest("SettingsStore initialized and settingsHandler called?", function () {
+        //start server
         gpii.integrationTesting.launchServer();
 
-        expect(2);
+        expect(3);
+        //send login token for user: integrationTester1
         http.get({
             host: 'localhost',
             port: 8081,
             path: "/user/integrationTester1/login"
         }, function(response) {
             //printRec(response);
-            console.log("Callbak from use login called");
+            console.log("Callback from use login called");
             ok(true, "It works");
 
+            //print out responses from server
             response.on('data', function (chunk) {
                 console.log('Response from server: ' + chunk);
             });
             response.on('close', function(ab) {
-                console.log("Closing time");
+                console.log("Connection closed by server");
                 start();
             });
+            //expect server to end connection on login
             response.on('end', function(ab) {
-                console.log("Ending time "+test);
+                console.log("Connection ended by server");
                 printRec(flowManager);
                 start();
             });
