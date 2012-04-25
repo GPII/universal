8fae07030821e8169ae6aba9c154e42b254af437
Yura Zenevich <yura.zenevich@gmail.com>
2012-04-25 12:42:30 -0400

    Passing integration test. Note: The gpii.integrationTesting.mockSettingsHandler might be doing something wrong.

--- tests/integrationTesting.js
+++ tests/integrationTesting.js
@@ -14,88 +14,21 @@ https://github.com/gpii/universal/LICENSE.txt
 /*global require*/
 
 (function () {
-    var fluid = require("universal");
-    var jqUnit = fluid.require("jqUnit");
-    
-    var gpii = fluid.registerNamespace("gpii");
-    var http = require("http");
+    // This loads universal.
+    var fluid = require("../gpii/index.js"),
+        http = require("http"),
+        fs = require("fs"),
+        os = require("os"),
+        gpii = fluid.registerNamespace("gpii"),
+        jqUnit = fluid.require("jqUnit");
+
+    fluid.require("./gpiiTests.js", require);
+
     gpii.integrationTesting = fluid.registerNamespace("gpii.integrationTesting");
-
-    //
-    var integrationTester1_expectedSettingsStore = [
-    {
-        "name": "[TESTING] Mock application",
-        "id": "net.gpii.testing.mock.linux.application",
-        "version": "3.2.1",
-        "contexts": {
-            "OS": {
-                "id": "linux",
-                "version": ">=2.6.26"
-            }
-        },
-        "settingsHandlers": [
-            {
-                "type": "gpii.integrationTesting.mockSettingsHandler",
-                "capabilitiesTransformations": {
-                    "screenEnhancement": {
-                        "magnification": "mag-factor",
-                        "tracking": {
-                            "expander": {
-                                "type": "gpii.transform.valueMapper",
-                                "path": "mouse-tracking",
-                                "options": {
-                                    "valueMap": {
-                                        "mouse": "centered"
-                                    }
-                                }
-                            }
-                        }
-                    }
-                },
-                "settings": {
-                    "applicationParameter1": true
-                }
-            }
-        ],
-        "launchHandlers": [
-            {
-                "type": "gpii.integrationTesting.mockLaunchHandler",
-                "options": {
-                    "start": [
-                        {
-                            "mockLaunchHandler-start": {
-                                "settings": {
-                                    "setting1": true
-                                },
-                                "options": {
-                                    "option1": true
-                                }
-                            }
-                        }
-                    ],
-                    "stop": [
-                        {
-                            "mockLaunchHandler-stop": {
-                                "settings": {
-                                    "setting1": false
-                                },
-                                "options": {
-                                    "setting1": false
-                                }
-                            }
-                        }
-                    ]
-                }
-            }
-        ]
-    }
-];
-
     gpii.integrationTesting.mockLaunchHandler = function(data) {
         jqUnit.assert("gpii.integrationTesting.mockLaunchHandler called");
         return data;
     };
-
     gpii.integrationTesting.mockSettingsHandler = function(data) {
         //fluid.log("MOCK SETTINGS HANDLER RECIEVED: "+JSON.stringify(data));
         jqUnit.assert("gpii.integrationTesting.mockSettingsHandler called");
@@ -113,42 +46,65 @@ https://github.com/gpii/universal/LICENSE.txt
         return cpy;
     };
 
-    var flowManager;
-    gpii.integrationTesting.launchServer = function() {
-        flowManager = gpii.flowManager();
+    fluid.demands("solutionsReporter", ["gpii.flowManager", "gpii.matchMaker", "gpii.test"], {
+        funcName: "gpii.dataSource.file",
+        args: {
+            url: "%db/test/data/solutions.reporter.mock1.json"
+        }
+    });
+    var integrationTester = gpii.tests.testEnvironment();
+
+    var getExpectedSettingsStore = function (url) {
+        var settings = fs.readFileSync(url, "utf8");
+        settings = JSON.parse(settings);
+        return fluid.remove_if(settings, function (setting, index) {
+            if (setting.contexts.OS.id !== os.platform()) {
+                return setting;
+            } else {
+                fluid.each(setting.settingsHandlers, function (settingsHandler) {
+                    settingsHandler.settings = {};
+                });
+            }
+        });
     };
 
-    jqUnit.asyncTest("Regular successful login", function () {
-        gpii.integrationTesting.launchServer();
-
+    integrationTester.asyncTest("Regular successful login", function () {
+        var flowManager = gpii.flowManager(),
+            expectedSettingsStore = getExpectedSettingsStore(flowManager.matchMakerDataSource.solutionsReporter.resolveUrl());
         jqUnit.expect(3);
         http.get({
             host: "localhost",
             port: 8081,
             path: "/user/integrationTester1/login"
         }, function(response) {
+            var data = "";
             fluid.log("Callback from use login called");
 
             response.on("data", function (chunk) {
                 fluid.log("Response from server: " + chunk);
+                data += chunk;
             });
-            response.on("close", function(dat) {
+            response.on("close", function(err) {
+                if (err) {
+                    fluid.log("Got error: " + err.message);
+                    jqUnit.start();
+                }
                 fluid.log("Connection to the server was closed");
-                jqUnit.start();
             });
-            response.on("end", function(dat) {
+            response.on("end", function() {
                 fluid.log("Connection to server ended");
                 if (flowManager && flowManager.launchManagerDataSource && 
                     flowManager.launchManagerDataSource.settingsStore) {
-                    jqUnit.assertDeepEq("Checking that the settingsStore contains the expected: ", 
-                        flowManager.launchManagerDataSource.settingsStore, integrationTester1_expectedSettingsStore);
+                    jqUnit.assertDeepEq("Checking that the settingsStore contains the expected: ", expectedSettingsStore,
+                        flowManager.launchManagerDataSource.settingsStore);
                 } else {
                    jqUnit.assert("flowManager or settingsStore are not set", false);
                 }
                 jqUnit.start();
             });
-        }).on('error', function(e) {
-            fluid.log("Got error: " + e.message);
+        }).on('error', function(err) {
+            fluid.log("Got error: " + err.message);
+            jqUnit.start();
         });
     });
     //TODO: assert payloads sent to handlers
