1904f0cc22c43a3dda527bd8c4bf5b6b51bbcacf
Ignacio Peinado <i.peinado.martinez@gmail.com>
2014-02-13 18:02:26 +0100

    GPII-320: added acceptance tests for Chrome extension

--- tests/all-tests.js
+++ tests/all-tests.js
@@ -31,6 +31,7 @@ var testIncludes = [
     "../gpii/node_modules/settingsHandlers/test/XMLSettingsHandlerTests.js",
     "../gpii/node_modules/settingsHandlers/test/INISettingsHandlerTests.js",
     "./acceptanceTests/AcceptanceTests_gnome_keyboard.js",
+    "./acceptanceTests/AcceptanceTests_chrome.js",
     "./DevelopmentTests.js",
 
 ];
