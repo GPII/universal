dfa0d28144a58230e16c3b197afdc9282feb0e76
Antranig Basman <antranig.basman@colorado.edu>
2014-08-27 00:19:57 +0100

    GPII-434: Added a bare driver file to universal

---
+++ gpii.js
@@ -0,0 +1,16 @@
+/*!
+GPII Universal Personalization Framework Node.js Bootstrap
+
+Copyright 2012 OCAD University
+
+Licensed under the New BSD license. You may not use this file except in
+compliance with this License.
+
+You may obtain a copy of the License at
+https://github.com/gpii/universal/LICENSE.txt
+*/
+
+var fluid = require("./gpii/index.js"),
+    gpii = fluid.registerNamespace("gpii");
+
+gpii.start();
