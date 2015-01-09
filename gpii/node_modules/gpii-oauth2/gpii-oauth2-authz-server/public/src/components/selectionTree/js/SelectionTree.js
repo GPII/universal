0f42207e0a51c731aab0c2855f2432fefa0bbadc
Justin Obara <obarajustin@gmail.com>
2015-01-09 15:31:10 -0500

    GPII-1051: forceCache for template

--- gpii/node_modules/gpii-oauth2/gpii-oauth2-authz-server/public/src/components/selectionTree/js/SelectionTree.js
+++ gpii/node_modules/gpii-oauth2/gpii-oauth2-authz-server/public/src/components/selectionTree/js/SelectionTree.js
@@ -43,7 +43,8 @@ var gpii = gpii || {};
         requestedPrefs: {},
         resources: {
             template: {
-                href: "../html/SelectionTreeTemplate.html"
+                href: "../html/SelectionTreeTemplate.html",
+                forceCache: true
             }
         },
         invokers: {
