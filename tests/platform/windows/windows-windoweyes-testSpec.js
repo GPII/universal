88ab3e3e55747d57875b3c2ecabdfe0a7e9c4ec2
Wayne <xpheal@gmail.com>
2015-06-18 15:23:45 -0500

    first magic commit

--- tests/platform/windows/windows-windoweyes-testSpec.js
+++ tests/platform/windows/windows-windoweyes-testSpec.js
@@ -1,10 +1,15 @@
 /*
+
 GPII Acceptance Testing
+
 Copyright 2014 Raising the Floor International
+
 Licensed under the New BSD license. You may not use this file except in
 compliance with this License.
+
 The research leading to these results has received funding from the European Union's
 Seventh Framework Programme (FP7/2007-2013) under grant agreement no. 289016.
+
 You may obtain a copy of the License at
 https://github.com/GPII/universal/blob/master/LICENSE.txt
 */
@@ -18,19 +23,21 @@ gpii.loadTestingSupport();
 
 fluid.registerNamespace("gpii.tests.windows");
 
-gpii.tests.windows.jaws = [
+gpii.tests.windows.windoweyes = [
     {
-        name: "Testing NP set \"jaws_application\" using Flat matchmaker",
-        userToken: "jaws_application",
+        name: "Testing NP set \"windoweyes_application\" using Flat matchmaker",
+        userToken: "windoweyes_application",
         settingsHandlers: {
-            "gpii.settingsHandlers.INISettingsHandler": {
+            "gpii.settingsHandlers.XMLHandler": {
                 "some.app.id": [
                     {
                         "settings": {
-                            "cloud4allVoiceProfile-GlobalContext.Speed": 115
+                            "we_settings.screen.voice.rate": 4,
+                            "we_settings.screen.voice.pitch": 3,
+                            "we_settings.hot_keys.cursor.key": {"hkid": "10","$t": "Control-Alt-Delete"}
                         },
                         "options": {
-                            "filename": "${{environment}.APPDATA}\\Freedom Scientific\\JAWS\\15.0\\Settings\\VoiceProfiles.ini"
+                            "filename": "${{environment}.APPDATA}\\GW Micro\\Window-Eyes\\users\\default\\sets.dat"
                         }
                     }
                 ]
@@ -38,24 +45,27 @@ gpii.tests.windows.jaws = [
         },
         processes: [
             {
-                "command": "tasklist /fi \"STATUS eq RUNNING\" /FI \"IMAGENAME eq jfw.exe\" | find /I \"jfw.exe\" /C",
+                "command": "tasklist /fi \"STATUS eq RUNNING\" /FI \"IMAGENAME eq wineyes.exe\" | find /I \"wineyes.exe\" /C",
                 "expectConfigured": "1",
                 "expectRestored": "0"
             }
         ]
-    }, {
-        name: "Testing NP set \"jaws_common\" using Flat matchmaker",
-        userToken: "jaws_common",
+    }, 
+
+    {
+        name: "Testing NP set \"windoweyes_common\" using Flat matchmaker",
+        userToken: "windoweyes_common",
         settingsHandlers: {
-            "gpii.settingsHandlers.INISettingsHandler": {
+            "gpii.settingsHandlers.XMLHandler": {
                 "some.app.id": [
                     {
                         "settings": {
-                            "cloud4allVoiceProfile-GlobalContext.Speed": 37.875,
-                            "cloud4allVoiceProfile-GlobalContext.Punctuation": 3
+                            "we_settings.screen.voice.rate": 4,
+                            "we_settings.screen.voice.pitch": 3,
+                            "we_settings.hot_keys.cursor.key": {"hkid": "10","$t": "Control-Alt-Delete"}
                         },
                         "options": {
-                            "filename": "${{environment}.APPDATA}\\Freedom Scientific\\JAWS\\15.0\\Settings\\VoiceProfiles.ini"
+                            "filename": "${{environment}.APPDATA}\\GW Micro\\Window-Eyes\\users\\default\\sets.dat"
                         }
                     }
                 ]
@@ -63,7 +73,7 @@ gpii.tests.windows.jaws = [
         },
         processes: [
             {
-                "command": "tasklist /fi \"STATUS eq RUNNING\" /FI \"IMAGENAME eq jfw.exe\" | find /I \"jfw.exe\" /C",
+                "command": "tasklist /fi \"STATUS eq RUNNING\" /FI \"IMAGENAME eq wineyes.exe\" | find /I \"wineyes.exe\" /C",
                 "expectConfigured": "1",
                 "expectRestored": "0"
             }
@@ -72,8 +82,8 @@ gpii.tests.windows.jaws = [
 ];
 
 module.exports = gpii.test.bootstrap({
-    testDefs:  "gpii.tests.windows.jaws",
-    configName: "windows-jaws-config",
+    testDefs:  "gpii.tests.windows.windoweyes",
+    configName: "windows-windoweyes-config",
     configPath: "configs"
 }, ["gpii.test.integration.testCaseHolder.windows"],
     module, require, __dirname);
