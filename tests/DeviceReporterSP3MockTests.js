2fe25eda688cdfa4c0a221e394cf149d3746860f
Kasper Markus <kasper2@markus.dk>
2015-08-05 13:12:16 +0200

    GPII-641: Temporary commit -- broken

--- tests/DeviceReporterSP3MockTests.js
+++ tests/DeviceReporterSP3MockTests.js
@@ -31,7 +31,27 @@ gpii.loadTestingSupport();
 // no errors are triggered
 fluid.registerNamespace("gpii.tests.deviceReporterMockChecks");
 
+fluid.defaults("gpii.tests.deviceReporterMockChecks.testCaseHolder", {
+    gradeNames: ["kettle.test.testCaseHolder", "autoInit"],
+    components: {
+        deviceRequest: {
+            type: "kettle.test.request.http",
+            options: {
+                path: "/device",
+                port: 8081
+            }
+        }
+    }
+});
+
 gpii.tests.deviceReporterMockChecks.userToken = "testUser1";
+
+gpii.tests.deviceReporterMockChecks.testDeviceErrorResponse = function (data) {
+        data = JSON.parse(data);
+        jqUnit.assertTrue("Received error as expected", data.isError);
+        jqUnit.assertEquals("Received error code 500", 500, data.statusCode);
+        // jqUnit.assertEquals("Received message as expected", "Error in device reporter data: Unexpected token o", data.message);
+};
 
 gpii.tests.deviceReporterMockChecks.testLoginResponse = function (data) {
         data = JSON.parse(data);
@@ -62,15 +82,15 @@ gpii.tests.deviceReporterMockChecks.popInstrumentedErrors = function () {
     // restore jqUnit's exception handler for the next test
     fluid.onUncaughtException.removeListener("fail");
 };
-gpii.tests.deviceReporterMockChecks.buildTestDef = function (path) {
-    return {
+gpii.tests.deviceReporterMockChecks.buildTestDef = function (reporterURL) {
+    return [{
         name: "Device Reporter faulty data tests",
         expect: 3,
         config: {
             configName: "development.all.local",
             configPath: configPath
         },
-        deviceReporterUrl: "file://" + path,
+        deviceReporterUrl: "file://" + reporterURL,
         gradeNames: [ "gpii.test.common.testCaseHolder" ],
         distributeOptions: [{
             "source": "{that}.options.deviceReporterUrl",
@@ -90,22 +110,32 @@ gpii.tests.deviceReporterMockChecks.buildTestDef = function (path) {
         }, {
             func: "gpii.tests.deviceReporterMockChecks.popInstrumentedErrors"
         }]
-    };
+    }, {
+        name: "Flow Manager development tests",
+        expect: 2,
+        config: {
+            configName: "deviceReporterOnly",
+            configPath: require("path").resolve(__dirname, "configs"),
+        },
+        deviceReporterUrl: "file://" + reporterURL,
+        gradeNames: [ "gpii.tests.deviceReporterMockChecks.testCaseHolder" ],
+        distributeOptions: [{
+            "source": "{that}.options.deviceReporterUrl",
+            "target": "{that deviceReporter}.options.installedSolutionsUrl"
+        }],
+        sequence: [{
+            func: "{deviceRequest}.send"
+        }, {
+            event: "{deviceRequest}.events.onComplete",
+            listener: "gpii.tests.deviceReporterMockChecks.testDeviceErrorResponse"
+        }]
+    }];
 };
 
 
 gpii.tests.deviceReporterMockChecks.buildAllTestDefs = function () {
-    var dirname = __dirname + "/data/";
-    var dirContent = fs.readdirSync(dirname);
-    var testDefs = [];
-
-    var regexp = new RegExp(".json$");
-    fluid.each(dirContent, function (filename) {
-        if (regexp.test(filename) && filename === "faultyDeviceReport.json") {
-            testDefs.push(gpii.tests.deviceReporterMockChecks.buildTestDef(dirname + filename));
-        }
-    });
-    return testDefs;
+    var filename = __dirname + "/data/faultyDeviceReport.json";
+    return gpii.tests.deviceReporterMockChecks.buildTestDef(filename);
 };
 
 kettle.test.bootstrapServer(gpii.tests.deviceReporterMockChecks.buildAllTestDefs());
