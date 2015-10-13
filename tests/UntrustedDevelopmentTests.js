c19b07740b342898c499d5a5195312cba1bd97d5
Simon Bates <simon@bitstructures.com>
2015-10-13 16:32:13 -0400

    GPII-1224: Continue UntrustedDevelopmentTests.js

--- tests/UntrustedDevelopmentTests.js
+++ tests/UntrustedDevelopmentTests.js
@@ -17,13 +17,15 @@
 "use strict";
 
 var fluid = require("infusion"),
+    jqUnit = fluid.require("jqUnit"),
     fs = require("fs"),
     path = require("path"),
+    gpii = fluid.registerNamespace("gpii"),
     kettle = fluid.registerNamespace("kettle");
 
 require("../index.js");
 
-// gpii.loadTestingSupport();
+gpii.loadTestingSupport();
 
 var untrustedFlowManagerConfig = fluid.copy(fluid.defaults(kettle.config.createDefaults({
     configName: "untrusted",
@@ -43,23 +45,105 @@ var config = {
     options: {
         gradeNames: "autoInit",
         components: {
-            localConfig: {
+            server: {
                 type: "fluid.eventedComponent",
-                options: untrustedFlowManagerConfig
-            },
-            cloudBasedConfig: {
-                type: "fluid.eventedComponent",
-                options: cloudBasedFlowManagerConfig
+                options: {
+                    gradeNames: "autoInit",
+                    components: {
+                        localConfig: {
+                            type: "fluid.eventedComponent",
+                            options: untrustedFlowManagerConfig
+                        },
+                        cloudBasedConfig: {
+                            type: "fluid.eventedComponent",
+                            options: cloudBasedFlowManagerConfig
+                        }
+                    },
+                    events: {
+                        onListen: {
+                            events: {
+                                onListenLocal: "{localConfig}.server.events.onListen",
+                                onListenCould : "{cloudBasedConfig}.server.events.onListen"
+                            }
+                        },
+                        onStopped: {
+                            events: {
+                                onStoppedLocal: "{localConfig}.server.events.onStopped",
+                                onStoppedCould : "{cloudBasedConfig}.server.events.onStopped"
+                            }
+                        }
+                    },
+                    invokers: {
+                        stop: {
+                            funcName: "gpii.tests.untrusted.development.stopServers",
+                            args: [ ["{localConfig}.server", "{cloudBasedConfig}.server"] ]
+                        }
+                    }
+                }
             }
         }
     }
 };
 
+fluid.registerNamespace("gpii.tests.untrusted.development");
+
+gpii.tests.untrusted.development.stopServers = function (servers) {
+    fluid.each(servers, function (server) {
+        server.stop();
+    });
+};
+
 fs.writeFileSync("UntrustedDevelopmentTestsConfig.json", JSON.stringify(config, null, 4));
 
-// TODO: Remove generated config after tests
+fluid.logObjectRenderChars = undefined;
 
-kettle.config.makeConfigLoader({
-    configName: "UntrustedDevelopmentTestsConfig",
-    configPath: __dirname
-});
+// *************************************
+// BEGIN Copied from DevelopmentTests.js
+// *************************************
+
+// TODO: Move shared parts of DevelopmentTests.js to a new file and re-use
+
+fluid.registerNamespace("gpii.tests.development");
+
+gpii.tests.development.userToken = "testUser1";
+
+gpii.tests.development.testLoginResponse = function (data) {
+    jqUnit.assertEquals("Response is correct", "User with token " +
+        gpii.tests.development.userToken + " was successfully logged in.", data);
+};
+
+gpii.tests.development.testLogoutResponse = function (data) {
+    jqUnit.assertEquals("Response is correct", "User with token " +
+        gpii.tests.development.userToken + " was successfully logged out.", data);
+};
+
+gpii.tests.development.testDefs = [{
+    name: "Flow Manager development tests",
+    expect: 2,
+    config: {
+        configName: "UntrustedDevelopmentTestsConfig",
+        configPath: __dirname
+    },
+    gradeNames: "gpii.test.common.testCaseHolder",
+    userToken: gpii.tests.development.userToken,
+
+    sequence: [{
+        func: "{loginRequest}.send"
+    }, {
+        event: "{loginRequest}.events.onComplete",
+        listener: "gpii.tests.development.testLoginResponse"
+    }, {
+        func: "{logoutRequest}.send"
+    }, {
+        event: "{logoutRequest}.events.onComplete",
+        listener: "gpii.tests.development.testLogoutResponse"
+    }]
+}];
+
+kettle.test.bootstrapServer(gpii.tests.development.testDefs);
+
+// *************************************
+// END Copied from DevelopmentTests.js
+// ***********************************
+
+// TODO: Remove generated config after tests
