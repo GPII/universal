6d795bce0d8eca4505ee16807e13f2c010e849b3
cstrobbe <c_strobbe-github@yahoo.co.uk>
2016-05-28 19:44:34 +0200

    EU funding ack move below licence URL.

--- tests/platform/cloud/AcceptanceTests_tvm.js
+++ tests/platform/cloud/AcceptanceTests_tvm.js
@@ -5,14 +5,14 @@ Copyright 2015,
     Fraunhofer IAO
     Hochschule der Medien (HdM) / Stuttgart Media University
 
-The research leading to these results has received funding from the European Union's
-Seventh Framework Programme (FP7/2007-2013) under grant agreement no. 289016.
-
 Licensed under the New BSD license. You may not use this file except in
 compliance with this License.
- 
+
 You may obtain a copy of the License at
 https://github.com/GPII/universal/blob/master/LICENSE.txt
+
+The research leading to these results has received funding from the European Union's
+Seventh Framework Programme (FP7/2007-2013) under grant agreement no. 289016.
 */
 
 /*global require*/
