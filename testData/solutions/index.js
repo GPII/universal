2a4f321f19215d7d0eeaf0ab013bb794143061b7
Kasper Markus <kasper2@markus.dk>
2016-06-08 16:48:27 +0200

    GPII-1809: Moved inverseCapabilities to solutionsRegistry

---
+++ testData/solutions/index.js
@@ -0,0 +1,5 @@
+exports.win32 = require("./win32.json");
+exports.linux = require("./linux.json");
+exports.darwin = require("./darwin.json");
+exports.android = require("./android.json");
+exports.web = require("./web.json");
