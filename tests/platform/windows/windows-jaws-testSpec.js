88eaf50ab76aa7ac9ac34a301ceb5d1872fc90b1
Wayne <xpheal@gmail.com>
2016-08-07 21:57:46 -0500

    add tests

--- tests/platform/windows/windows-jaws-testSpec.js
+++ tests/platform/windows/windows-jaws-testSpec.js
@@ -69,7 +69,6 @@ gpii.tests.windows.jaws = [
             }
         ]
     }, 
-
     {
         name: "Testing NP set \"jaws_common\" using Flat matchmaker",
         userToken: "jaws_common",
@@ -123,6 +122,143 @@ gpii.tests.windows.jaws = [
                             "options.SayAllIndicateCaps": false,
                             "options.SayAllIgnoreShiftKeys": false,
                             "options.TypingEcho": 3
+                        },
+                        "options": {
+                            "filename": "${{environment}.APPDATA}\\Freedom Scientific\\JAWS\\17.0\\Settings\\enu\\DEFAULT.JCF"
+                        }
+                    }
+                ]
+            }
+        },
+        processes: [
+            {
+                "command": "tasklist /fi \"STATUS eq RUNNING\" /FI \"IMAGENAME eq jfw.exe\" | find /I \"jfw.exe\" /C",
+                "expectConfigured": "1",
+                "expectRestored": "0"
+            }
+        ]
+    },
+    {
+        name: "Testing NP set \"jaws_common2\" using Flat matchmaker",
+        userToken: "jaws_common2",
+        settingsHandlers: {
+            "gpii.settingsHandlers.INISettingsHandler": {
+                "some.app.id": [
+                    {
+                        "settings": {
+                            "Options.PrimarySynthesizer": "eloq",
+                            "enu-Global.Rate": 40,
+                            "enu-Global.Punctuation": 0,
+                            "enu-Global.Pitch": 11,
+                            "enu-Global.Volume": 10,
+                            "enu-Global.SynthLangString": "Italian",
+                            "enu-Message.Rate": 40,
+                            "enu-Message.Punctuation": 0,
+                            "enu-Message.Pitch": 11,
+                            "enu-Message.Volume": 10,
+                            "enu-Message.SynthLangString": "Italian",
+                            "enu-Keyboard.Rate": 40,
+                            "enu-Keyboard.Punctuation": 0,
+                            "enu-Keyboard.Pitch": 11,
+                            "enu-Keyboard.Volume": 10,
+                            "enu-Keyboard.SynthLangString": "Italian",
+                            "enu-PCCursor.Rate": 40,
+                            "enu-PCCursor.Punctuation": 0,
+                            "enu-PCCursor.Pitch": 11,
+                            "enu-PCCursor.Volume": 10,
+                            "enu-PCCursor.SynthLangString": "Italian",
+                            "enu-JAWSCursor.Rate": 40,
+                            "enu-JAWSCursor.Punctuation": 0,
+                            "enu-JAWSCursor.Pitch": 11,
+                            "enu-JAWSCursor.Volume": 10,
+                            "enu-JAWSCursor.SynthLangString": "Italian",
+                            "enu-MenuAndDialog.Rate": 40,
+                            "enu-MenuAndDialog.Punctuation": 0,
+                            "enu-MenuAndDialog.Pitch": 11,
+                            "enu-MenuAndDialog.Volume": 10,
+                            "enu-MenuAndDialog.SynthLangString": "Italian"
+                        },
+                        "options": {
+                            "filename": "${{environment}.APPDATA}\\Freedom Scientific\\JAWS\\17.0\\Settings\\VoiceProfiles\\GPII.VPF"
+                        }
+                    },
+
+                    {
+                        "settings": {
+                            "Voice Profiles.ActiveVoiceProfileName": "GPII",
+                            "Braille.BrailleMode": 0,
+                            "options.SayAllMode": 0,
+                            "options.SayAllIndicateCaps": false,
+                            "options.SayAllIgnoreShiftKeys": false
+                        },
+                        "options": {
+                            "filename": "${{environment}.APPDATA}\\Freedom Scientific\\JAWS\\17.0\\Settings\\enu\\DEFAULT.JCF"
+                        }
+                    }
+                ]
+            }
+        },
+        processes: [
+            {
+                "command": "tasklist /fi \"STATUS eq RUNNING\" /FI \"IMAGENAME eq jfw.exe\" | find /I \"jfw.exe\" /C",
+                "expectConfigured": "1",
+                "expectRestored": "0"
+            }
+        ]
+    },
+    {
+        name: "Testing NP set \"jaws_common3\" using Flat matchmaker",
+        userToken: "jaws_common3",
+        settingsHandlers: {
+            "gpii.settingsHandlers.INISettingsHandler": {
+                "some.app.id": [
+                    {
+                        "settings": {
+                            "Options.PrimarySynthesizer": "eloq",
+                            "enu-Global.Rate": 100,
+                            "enu-Global.Punctuation": 2,
+                            "enu-Global.Pitch": 100,
+                            "enu-Global.Volume": 100,
+                            "enu-Global.SynthLangString": "French Canadian",
+                            "enu-Message.Rate": 100,
+                            "enu-Message.Punctuation": 2,
+                            "enu-Message.Pitch": 100,
+                            "enu-Message.Volume": 100,
+                            "enu-Message.SynthLangString": "French Canadian",
+                            "enu-Keyboard.Rate": 100,
+                            "enu-Keyboard.Punctuation": 2,
+                            "enu-Keyboard.Pitch": 100,
+                            "enu-Keyboard.Volume": 100,
+                            "enu-Keyboard.SynthLangString": "French Canadian",
+                            "enu-PCCursor.Rate": 100,
+                            "enu-PCCursor.Punctuation": 2,
+                            "enu-PCCursor.Pitch": 100,
+                            "enu-PCCursor.Volume": 100,
+                            "enu-PCCursor.SynthLangString": "French Canadian",
+                            "enu-JAWSCursor.Rate": 100,
+                            "enu-JAWSCursor.Punctuation": 2,
+                            "enu-JAWSCursor.Pitch": 100,
+                            "enu-JAWSCursor.Volume": 100,
+                            "enu-JAWSCursor.SynthLangString": "French Canadian",
+                            "enu-MenuAndDialog.Rate": 100,
+                            "enu-MenuAndDialog.Punctuation": 2,
+                            "enu-MenuAndDialog.Pitch": 100,
+                            "enu-MenuAndDialog.Volume": 100,
+                            "enu-MenuAndDialog.SynthLangString": "French Canadian"
+                        },
+                        "options": {
+                            "filename": "${{environment}.APPDATA}\\Freedom Scientific\\JAWS\\17.0\\Settings\\VoiceProfiles\\GPII.VPF"
+                        }
+                    },
+
+                    {
+                        "settings": {
+                            "Voice Profiles.ActiveVoiceProfileName": "GPII",
+                            "Braille.BrailleMode": 2,
+                            "options.SayAllMode": 2,
+                            "options.SayAllIndicateCaps": true,
+                            "options.SayAllIgnoreShiftKeys": true,
+                            "options.TypingEcho": 1
                         },
                         "options": {
                             "filename": "${{environment}.APPDATA}\\Freedom Scientific\\JAWS\\17.0\\Settings\\enu\\DEFAULT.JCF"
