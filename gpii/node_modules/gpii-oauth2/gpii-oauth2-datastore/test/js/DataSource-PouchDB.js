12e1b3f179a3b55abe1da35c633611f2f86339e5
Cindy Qi Li <cli@ocadu.ca>
2016-09-16 16:55:07 -0400

    GPII-1872: In progress of creating the pouchDB data source to support the in-browser testing for the auth server. Able to handle GET requests by document ids or views. Need to implement the SET handler.

--- gpii/node_modules/gpii-oauth2/gpii-oauth2-datastore/test/js/DataSource-PouchDB.js
+++ gpii/node_modules/gpii-oauth2/gpii-oauth2-datastore/test/js/DataSource-PouchDB.js
@@ -19,55 +19,96 @@ var fluid = fluid || require("infusion");
 
     "use strict";
 
-    fluid.defaults("gpii.tests.dataSource.PouchDB", {
-        mergePolicy: {
-            "rules": "nomerge"
-        },
-        rules: {
-            writePayload: {
-                value: ""
-            },
-            readPayload: {
-                "": "value"
-            }
-        },
-        components: {
-            pouchDb: {
-                type: "gpii.pouch",
-                options: {
-                    dbOptions: {
-                        name: null   // provided by integrator
-                    }
-                }
+    fluid.defaults("gpii.dataSource.pouchDB", {
+        invokers: {
+            getImpl: {
+                funcName: "gpii.dataSource.pouchDB.handle",
+                args: ["{that}", "{gpii.pouch}", "{arguments}.0", "{arguments}.1"] // options, directModel
             }
         },
         listeners: {
-            onRead: {
-                funcName: "gpii.tests.dataSource.PouchDB.read",
-                args: ["{that}", "{arguments}.0"], // resp
-                namespace: "PouchDB",
-                priority: "after:encoding"
-            }
+            onRead: [{
+                funcName: "fluid.identity",
+                namespace: "encoding"
+            }]
         }
     });
 
-    fluid.defaults("gpii.tests.dataSource.PouchDB.writable", {
+    fluid.defaults("gpii.dataSource.pouchDB.writable", {
         listeners: {
             onWrite: {
-                funcName: "gpii.tests.dataSource.PouchDB.write",
+                funcName: "gpii.dataSource.pouchDB.write",
                 args: ["{that}", "{arguments}.0", "{arguments}.1"], // model, options
-                namespace: "PouchDB",
+                namespace: "pouchDB",
                 priority: "after:encoding"
             }
         }
     });
 
-    fluid.makeGradeLinkage("gpii.tests.dataSource.PouchDB.linkage", ["kettle.dataSource.writable", "gpii.tests.dataSource.PouchDB"], "gpii.tests.dataSource.PouchDB.writable");
+    fluid.makeGradeLinkage("gpii.dataSource.pouchDB.linkage", ["gpii.dataSource.writable", "gpii.dataSource.pouchDB"], "gpii.dataSource.pouchDB.writable");
 
-    gpii.tests.dataSource.PouchDB.read = function (that, resp) {
+    // Matching the url with the _id field of the view list to determine if this url is to query by a view or an document id
+    gpii.dataSource.pouchDB.isQueryView = function (dbViews, url) {
+        var viewIdentifier = dbViews[0]._id;
+        return url.indexOf(viewIdentifier) !== -1;
     };
 
-    gpii.tests.dataSource.PouchDB.write = function (that, model, options) {
+    gpii.dataSource.pouchDB.handle = function (that, pouchDB, options, directModel) {
+        var url = that.resolveUrl(that.options.requestUrl, that.options.termMap, directModel);
+        return gpii.dataSource.pouchDB.handle.pouchDB(that, pouchDB, url);
+    };
+
+    // Decode the URL to find out the view/map function name and the parameters for the function.
+    // Decoding an URL: /_design/views/_view/findAuthByClientCredentialsAccessToken?key=%22firstDiscovery_access_token%22&include_docs=true
+    // outputs an object:
+    // {
+    //     viewName: "findAuthByClientCredentialsAccessToken",
+    //     viewOptions: {
+    //         key: "firstDiscovery_access_token",
+    //         include_docs: true
+    //     }
+    // }
+    gpii.dataSource.pouchDB.decodeView = function (url) {
+        var match = url.match(/(.*)\/(.*)\?(.*)/);
+
+        var params = decodeURI(match[3]).split("&");
+        var viewOptions = {};
+
+        fluid.each(params, function (param) {
+            var key, value;
+            [key, value] = param.split("=");
+            value = key === "include_docs" ? Boolean(value) : value.replace(/\"/g, "");
+            viewOptions[key] = value;
+        });
+
+        return {
+            viewName: match[2],
+            viewOptions: viewOptions
+        };
+    };
+
+    gpii.dataSource.pouchDB.handle.pouchDB = function (that, pouchDB, url) {
+        var dbViews = that.options.dbViews;
+        var promiseTogo = fluid.promise();
+
+        var isQueryView = gpii.dataSource.pouchDB.isQueryView(dbViews, url);
+        if (isQueryView) {
+            var decodedViewInfo = gpii.dataSource.pouchDB.decodeView(url);
+            var viewList = dbViews[0].views;
+            var viewFunc = viewList[decodedViewInfo.viewName];
+
+            promiseTogo = pouchDB.query(viewFunc, decodedViewInfo.viewOptions);
+        } else {
+            var id = url.substring(1);
+            promiseTogo = pouchDB.get(id);
+        }
+        // promiseTogo.then(function (data) {
+        //     console.log("data", data);
+        // });
+        return promiseTogo;
+    };
+
+    gpii.dataSource.pouchDB.write = function (that, model, options) {
     };
 
 })();
--- gpii/node_modules/gpii-oauth2/gpii-oauth2-datastore/test/js/DbDataStoreTests.js
+++ gpii/node_modules/gpii-oauth2/gpii-oauth2-datastore/test/js/DbDataStoreTests.js
@@ -18,72 +18,139 @@ var fluid = fluid || require("infusion"),
 
     "use strict";
 
-    fluid.defaults("gpii.tests.oauth2.dbDataStore", {
-        gradeNames: ["gpii.oauth2.dbDataStore"],
-        distributeOptions: {
-            "pouchDBGrade": {
-                record: "gpii.tests.dataSource.PouchDB",
-                target: "{that gpii.oauth2.dbDataSource}.options.gradeNames"
-            },
-            "dbName": {
-                source: "{that}.options.dataSourceConfig.dbName",
-                target: "{that > gpii.tests.dataSource.PouchDB}.options.components.pouchDb.options.dbOptions.name"
-            }
-        }
-    });
-
-    fluid.defaults("gpii.tests.oauth2.dbDataStoreWithViews", {
+    fluid.defaults("gpii.tests.oauth2.pouchBackedDataStore", {
         gradeNames: ["fluid.component"],
         dbViewsLocation: null,   // provided by integrators
+        pouchData: null,   // provided by integrators
+        dbName: "auth",    // Configurable by integrators
         components: {
-            dbViewsLoader: {
+            dataLoader: {
                 type: "fluid.component",
                 options: {
                     resources: {
                         dbViews: {
-                            href: "{dbDataStoreWithViews}.options.dbViewsLocation",
+                            href: "{pouchBackedDataStore}.options.dbViewsLocation",
                             options: {
                                 dataType: "json"
                             }
                         }
                     },
                     events: {
-                        onDbViewsLoaded: "{dbDataStoreWithViews}.events.onDbViewsLoaded"
+                        onDbViewsLoaded: "{pouchBackedDataStore}.events.onDbViewsLoaded"
                     },
                     listeners: {
                         "onCreate.fetchDbViews": {
-                            listener: "gpii.tests.oauth2.dbDataStoreWithViews.fetchDbViews",
+                            listener: "gpii.tests.oauth2.pouchBackedDataStore.fetchDbViews",
                             args: ["{that}"]
                         }
                     }
                 }
             },
-            dbDataStore: {
-                type: "gpii.tests.oauth2.dbDataStore",
-                createOnEvent: "onDbViewsLoaded",
+            pouchDb: {
+                type: "gpii.pouch",
                 options: {
-                    dbViews: "{arguments}.0"
+                    dbOptions: {
+                        name: "{pouchBackedDataStore}.options.dbName"
+                    },
+                    data: "{pouchBackedDataStore}.options.pouchData",
+                    listeners: {
+                        "onCreate.populateData": {
+                            listener: "{that}.bulkDocs",
+                            args: ["{that}.options.data"]
+                        },
+                        "onBulkDocsComplete.escalate": "{pouchBackedDataStore}.events.onPouchReady"
+                    }
+                }
+            },
+            dbDataStore: {
+                type: "gpii.oauth2.dbDataStore",
+                createOnEvent: "onCreateDataStore",
+                options: {
+                    dbViews: "{arguments}.0",
+                    distributeOptions: {
+                        "pouchDBGrade": {
+                            record: "gpii.dataSource.pouchDB",
+                            target: "{that gpii.oauth2.dbDataSource}.options.gradeNames"
+                        },
+                        "dbViews": {
+                            record: "{that}.options.dbViews",
+                            target: "{that gpii.oauth2.dbDataSource}.options.dbViews"
+                        }
+                    },
+                    listeners: {
+                        "onCreate.test": {
+                            listener: "gpii.tests.oauth2.pouchBackedDataStore.test",
+                            args: ["{that}"]
+                        }
+                    }
                 }
             }
         },
         events: {
-            onDbViewsLoaded: null
+            onDbViewsLoaded: null,
+            onPouchReady: null,
+            onCreateDataStore: {
+                events: {
+                    onDbViewsLoaded: "onDbViewsLoaded",
+                    onPouchReady: "onPouchReady"
+                },
+                args: ["{dataLoader}.dbViews"]
+            }
         }
     });
 
-    gpii.tests.oauth2.dbDataStoreWithViews.fetchDbViews = function (that) {
+    gpii.tests.oauth2.pouchBackedDataStore.fetchDbViews = function (that) {
         fluid.fetchResources(that.options.resources, function (resourceSpecs) {
+            that.dbViews = resourceSpecs.dbViews.resourceText;
             that.events.onDbViewsLoaded.fire(resourceSpecs.dbViews.resourceText);
         });
     };
 
-    var dbDataStore = gpii.tests.oauth2.dbDataStoreWithViews({
+    var pouchBackedDataStore = gpii.tests.oauth2.pouchBackedDataStore({
         dbViewsLocation: "../../dbViews/views.json",
-        dataSourceConfig: {
-            dbName: "auth"
-        }
+        pouchData: [
+            {
+                "_id": "user-1",
+                "type": "user",
+                "name": "alice",
+                "password": "a",
+                "defaultGpiiToken": "alice_gpii_token"
+            },
+            {
+                "_id": "gpiiToken-1",
+                "type": "gpiiToken",
+                "gpiiToken": "alice_gpii_token",
+                "userId": "user-1"
+            },
+            {
+                "_id": "client-2",
+                "type": "client",
+                "name": "First Discovery",
+                "oauth2ClientId": "net.gpii.prefsEditors.firstDiscovery",
+                "oauth2ClientSecret": "client_secret_firstDiscovery",
+                "allowDirectGpiiTokenAccess": true,
+                "allowAddPrefs": true
+            },
+            {
+                "_id": "clientCredentialsToken-1",
+                "type": "clientCredentialsToken",
+                "clientId": "client-2",
+                "accessToken": "firstDiscovery_access_token",
+                "allowAddPrefs": true,
+                "revoked": false
+            }
+        ],
+        dbName: "auth"
     });
 
-    console.log(dbDataStore);
+    gpii.tests.oauth2.pouchBackedDataStore.test = function (that) {
+        // var promiseTogo = that.findUserById("user-1");
+        var promiseTogo = that.findAuthByClientCredentialsAccessToken("firstDiscovery_access_token");
+        promiseTogo.then(function (result) {
+            console.log("result: ", result);
+        }, function (err) {
+            console.log("err: ", err);
+        });
+    };
 
 })();
