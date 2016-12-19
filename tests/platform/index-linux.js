5f16f87b62426089fdc5ae94acabb1ef739dbde2
Javier Hernández <javi@raisingthefloor.org>
2016-12-19 18:11:14 +0100

    GPII-2173: Removed com.ilunion.cloud4chrome and related tests

--- tests/platform/index-linux.js
+++ tests/platform/index-linux.js
@@ -23,6 +23,5 @@ https://github.com/GPII/universal/blob/master/LICENSE.txt
 module.exports = [
     "linux/linux-builtIn-testSpec.js",
     "linux/linux-orca-testSpec.js",
-    "linux/linux-chrome-testSpec.js",
     "linux/linux-dynamicDeviceReporter-testSpec.js"
 ];
