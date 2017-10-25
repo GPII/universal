b66e158cc98cd1a96337cdb4cdba5d8653a5f990
Cindy Qi Li <cli@ocadu.ca>
2017-10-25 14:27:31 -0400

    GPII-2662: Start to remove the oauth support for authorization code grant and client credentials grant.

--- gpii/node_modules/gpii-oauth2/gpii-oauth2-authz-server/src/AuthGrantFinder.js
+++ gpii/node_modules/gpii-oauth2/gpii-oauth2-authz-server/src/AuthGrantFinder.js
@@ -39,9 +39,7 @@ var fluid = fluid || require("infusion");
     });
 
     // Return a promise object that contains the granted privilege for the access token.
-    // This function looks up access tokens granted for privileged prefs creators and
-    // web prefs consumers to find the match. The different data structure
-    // can be returned based on the client/authorization type.
+    // This function looks up access tokens granted for GPII app installations to find the match.
     gpii.oauth2.authGrantFinder.getGrantForAccessToken = function (authorizationService, accessToken) {
         var promiseTogo = fluid.promise();
         var authorizationPromise = authorizationService.getAuthorizationByAccessToken(accessToken);
@@ -58,27 +56,10 @@ var fluid = fluid || require("infusion");
                         allowUntrustedPreferences: true
                     };
                 }
-
-                if (authRecord.authorization.type === gpii.oauth2.docTypes.privilegedPrefsCreatorAuthorization) {
-                    grant = {
-                        accessToken: accessToken,
-                        allowAddPrefs: true
-                    };
-                }
-
-                if (authRecord.authorization.type === gpii.oauth2.docTypes.webPrefsConsumerAuthorization) {
-                    grant = {
-                        accessToken: accessToken,
-                        gpiiToken: authRecord.authorization.gpiiToken,
-                        selectedPreferences: authRecord.authorization.selectedPreferences,
-                        oauth2ClientId: authRecord.client.oauth2ClientId
-                    };
-                }
             }
             promiseTogo.resolve(grant);
         });
 
         return promiseTogo;
     };
-
 })();
--- gpii/node_modules/gpii-oauth2/gpii-oauth2-authz-server/src/ClientService.js
+++ gpii/node_modules/gpii-oauth2/gpii-oauth2-authz-server/src/ClientService.js
@@ -4,9 +4,6 @@ Copyright 2014 OCAD university
 Licensed under the New BSD license. You may not use this file except in
 compliance with this License.
 
-The research leading to these results has received funding from the European Union's
-Seventh Framework Programme (FP7/2007-2013) under grant agreement no. 289016.
-
 You may obtain a copy of the License at
 https://github.com/GPII/universal/blob/master/LICENSE.txt
 */
@@ -27,14 +24,9 @@ fluid.defaults("gpii.oauth2.clientService", {
     },
     invokers: {
         authenticateClient: {
-            funcName: "gpii.oauth2.clientService.processClient",
-            args: ["{dataStore}", "{arguments}.0", "oauth2ClientSecret", "{arguments}.1"]
+            funcName: "gpii.oauth2.clientService.authenticateClient",
+            args: ["{dataStore}", "{arguments}.0", "{arguments}.1"]
                 // oauth2ClientId, oauth2ClientSecret
-        },
-        checkWebPrefsConsumerRedirectUri: {
-            funcName: "gpii.oauth2.clientService.processClient",
-            args: ["{dataStore}", "{arguments}.0", "redirectUri", "{arguments}.1"]
-                // oauth2ClientId, redirectUri
         },
         getClientById: {
             func: "{dataStore}.findClientById"
@@ -45,18 +37,15 @@ fluid.defaults("gpii.oauth2.clientService", {
 // To verify a client information matches the expected value
 //
 // @dataStore (Object) - an instance of a data store component such as gpii.oauth2.dbDataStore
-// @oauth2ClientId (String) - an oAuth2 client id
-// @fieldToVerify (String) - A field of the client object to be verified:
-// in authenticateClient() API, this field is "oauth2ClientSecret"; in checkWebPrefsConsumerRedirectUri() API,
-// this field is "redirectUri"
-// @expectedVerifiedValue (Any) - The expected verified value
+// @oauth2ClientId (String) - an OAuth2 client id
+// @expectedOauth2ClientSecret (String) - The expected OAuth2 client secret
 //
 // @return (Promise) - A promise object that contains either a client record or an error
-gpii.oauth2.clientService.processClient = function (dataStore, oauth2ClientId, fieldToVerify, expectedVerifiedValue) {
+gpii.oauth2.clientService.authenticateClient = function (dataStore, oauth2ClientId, expectedOauth2ClientSecret) {
     var promiseTogo = fluid.promise();
     var clientPromise = dataStore.findClientByOauth2ClientId(oauth2ClientId);
     clientPromise.then(function (client) {
-        if (client && client[fieldToVerify] === expectedVerifiedValue) {
+        if (client && fluid.get(client, ["oauth2ClientSecret"]) === expectedOauth2ClientSecret) {
             promiseTogo.resolve(client);
         } else {
             fluid.log("clientService: unauthorized client with oauth2ClientId - " + oauth2ClientId);
