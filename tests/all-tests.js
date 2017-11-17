1e510633d951276a426a68067f1827d5586a5c69
ste <git@grundell.co.uk>
2017-11-17 14:24:25 +0000

    GPII-2294: Stupid typo

--- tests/all-tests.js
+++ tests/all-tests.js
@@ -75,7 +75,7 @@ var testIncludes = [
     "../gpii/node_modules/contextManager/test/ContextManagerTests.js",
     "../gpii/node_modules/singleInstance/test/SingleInstanceTests.js",
     "../gpii/node_modules/eventLog/test/EventLogTests.js",
-    "../gpii/node_modules/userListeners/tests/all-tests.js"
+    "../gpii/node_modules/userListeners/test/all-tests.js"
 ];
 
 fluid.each(testIncludes, function (path) {
