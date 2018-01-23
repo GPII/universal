72f4e3c8862cc6ac9a8f58bc5476906f161b1da2
Cindy Qi Li <cli@ocadu.ca>
2018-01-23 10:13:33 -0500

    GPII-2630: Changes to the test VM that spins up GPII in the production config.

--- vagrant-configs/provisioning/deploy.sh
+++ vagrant-configs/provisioning/deploy.sh
@@ -8,19 +8,10 @@ sudo ansible-galaxy install -fr /home/vagrant/sync/vagrant-configs/provisioning/
 # Install CouchDB
 sudo PYTHONUNBUFFERED=1 ansible-playbook /home/vagrant/sync/vagrant-configs/provisioning/couchdb.yml --tags="install,configure,deploy"
 
-# Install Preferences Server
-sudo PYTHONUNBUFFERED=1 ansible-playbook /home/vagrant/sync/vagrant-configs/provisioning/preferences-server.yml --tags="install,configure,deploy"
-
 # Install Flow Manager
 sudo PYTHONUNBUFFERED=1 ansible-playbook /home/vagrant/sync/vagrant-configs/provisioning/flow-manager.yml --tags="install,configure,deploy"
 
 # Add additional CouchDB data
-"%universal/testData/dbData/clientCredentials.json",
-"%universal/testData/dbData/gpiiAppInstallationClients.json",
-"%universal/testData/dbData/gpiiKeys.json",
-"%universal/testData/dbData/prefsSafes.json",
-"%universal/testData/dbData/views.json"
-
 kanso upload /home/vagrant/sync/testData/dbData/clientCredentials.json http://localhost:5984/gpii
 kanso upload /home/vagrant/sync/testData/dbData/gpiiAppInstallationClients.json http://localhost:5984/gpii
 kanso upload /home/vagrant/sync/testData/dbData/gpiiKeys.json http://localhost:5984/gpii
