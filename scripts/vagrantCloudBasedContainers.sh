7afd54a5b291bd01525959a024a859f7db63e57e
Cindy Qi Li <cli@ocadu.ca>
2018-02-08 16:09:29 -0500

    GPII-2630: Modified the shell script that builds the docker image in the production config.

--- scripts/vagrantCloudBasedContainers.sh
+++ scripts/vagrantCloudBasedContainers.sh
@@ -3,10 +3,9 @@
 # This scripts deploys an environment for end-to-end tests based on Docker
 # containers. It was initially developed to support CI.
 #
-# It builds a Docker image for GPII/universal and uses it to start two
-# components: the Preferences Server and the Flow Manager.
+# It builds a Docker image for GPII/universal and uses it to start the Flow Manager.
 #
-# It also starts a CouchDB container and loads the Preferences data into
+# It also starts a CouchDB container and loads the CouchDB data into
 # it, so tests running against the GPII components will have access to the
 # latest test data.
 #
@@ -22,18 +21,20 @@ COUCHDB_PORT=5984
 COUCHDB_HEALTHCHECK_DELAY=5
 COUCHDB_HEALTHCHECK_TIMEOUT=30
 
+DBDATA_DIR="/home/vagrant/sync/universal/testData/dbData"
+
 DATALOADER_IMAGE="gpii/preferences-dataloader"
 DATALOADER_CLEAR_INDEX=1
-DATALOADER_COUCHDB_URL="http://couchdb:${COUCHDB_PORT}/preferences"
+DATALOADER_COUCHDB_URL="http://couchdb:${COUCHDB_PORT}/gpii"
 
-PREFERENCES_CONFIG="gpii.config.preferencesServer.standalone.production"
-PREFERENCES_DATASOURCE_URL="http://couchdb:${COUCHDB_PORT}/preferences/%gpiiKey"
-PREFERENCES_DIR="/home/vagrant/sync/universal/testData/preferences"
-PREFERENCES_PORT=9081
+# PREFERENCES_CONFIG="gpii.config.preferencesServer.standalone.production"
+# PREFERENCES_DATASOURCE_URL="http://couchdb:${COUCHDB_PORT}/preferences/%gpiiKey"
+# PREFERENCES_DIR="/home/vagrant/sync/universal/testData/preferences"
+# PREFERENCES_PORT=9081
 
 FLOWMANAGER_CONFIG="gpii.config.cloudBased.production"
-FLOWMANAGER_PREFERENCES_URL="http://preferences:${PREFERENCES_PORT}/preferences/%gpiiKey"
-FLOWMANAGER_PORT=9082
+# FLOWMANAGER_PREFERENCES_URL="http://preferences:${PREFERENCES_PORT}/preferences/%gpiiKey"
+FLOWMANAGER_PORT=9081
 
 
 # Remove old containers (exit code is ignored)
@@ -54,6 +55,6 @@ docker run -d -p $COUCHDB_PORT:$COUCHDB_PORT --name couchdb $COUCHDB_IMAGE
 sleep $COUCHDB_HEALTHCHECK_DELAY
 curl --retry $COUCHDB_HEALTHCHECK_TIMEOUT --retry-delay 1 --retry-connrefused http://localhost:$COUCHDB_PORT
 
-docker run -d -p $PREFERENCES_PORT:$PREFERENCES_PORT --name preferences --link couchdb -e NODE_ENV=$PREFERENCES_CONFIG -e GPII_PREFERENCES_DATASOURCE_URL=$PREFERENCES_DATASOURCE_URL -e GPII_PREFERENCES_LISTEN_PORT=$PREFERENCES_PORT $UNIVERSAL_IMAGE
-docker run -d -p $FLOWMANAGER_PORT:$FLOWMANAGER_PORT --name flowmanager --link preferences -e NODE_ENV=$FLOWMANAGER_CONFIG -e GPII_FLOWMANAGER_PREFERENCES_URL=$FLOWMANAGER_PREFERENCES_URL -e GPII_FLOWMANAGER_LISTEN_PORT=$FLOWMANAGER_PORT $UNIVERSAL_IMAGE
-docker run --rm --link couchdb -v $PREFERENCES_DIR:/data -e PREFERENCES_DIR=/data -e COUCHDB_URL=$DATALOADER_COUCHDB_URL -e CLEAR_INDEX=$DATALOADER_CLEAR_INDEX $DATALOADER_IMAGE
+# docker run -d -p $PREFERENCES_PORT:$PREFERENCES_PORT --name preferences --link couchdb -e NODE_ENV=$PREFERENCES_CONFIG -e GPII_PREFERENCES_DATASOURCE_URL=$PREFERENCES_DATASOURCE_URL -e GPII_PREFERENCES_LISTEN_PORT=$PREFERENCES_PORT $UNIVERSAL_IMAGE
+docker run -d -p $FLOWMANAGER_PORT:$FLOWMANAGER_PORT --name flowmanager -e NODE_ENV=$FLOWMANAGER_CONFIG -e GPII_FLOWMANAGER_LISTEN_PORT=$FLOWMANAGER_PORT $UNIVERSAL_IMAGE
+docker run --rm --link couchdb -v $DBDATA_DIR:/data -e DBDATA_DIR=/data -e COUCHDB_URL=$DATALOADER_COUCHDB_URL -e CLEAR_INDEX=$DATALOADER_CLEAR_INDEX $DATALOADER_IMAGE
