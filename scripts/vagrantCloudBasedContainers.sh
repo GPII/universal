1430480e05e5b6789feb63f801e6a3f9b3187cfb
Cindy Qi Li <cli@ocadu.ca>
2018-02-13 16:22:25 -0500

    GPII-2630: Modified scripts/vagrantCloudBasedContainers.sh and related config files to build proper docker containers for GPII-2630 changes.

--- scripts/vagrantCloudBasedContainers.sh
+++ scripts/vagrantCloudBasedContainers.sh
@@ -27,15 +27,10 @@ DATALOADER_IMAGE="cindyqili/gpii-dataloader"
 DATALOADER_CLEAR_INDEX=1
 DATALOADER_COUCHDB_URL="http://couchdb:${COUCHDB_PORT}/gpii"
 
-# PREFERENCES_CONFIG="gpii.config.preferencesServer.standalone.production"
-# PREFERENCES_DATASOURCE_URL="http://couchdb:${COUCHDB_PORT}/preferences/%gpiiKey"
-# PREFERENCES_DIR="/home/vagrant/sync/universal/testData/preferences"
-# PREFERENCES_PORT=9081
-
 FLOWMANAGER_CONFIG="gpii.config.cloudBased.production"
-# FLOWMANAGER_PREFERENCES_URL="http://preferences:${PREFERENCES_PORT}/preferences/%gpiiKey"
 FLOWMANAGER_PORT=9081
-
+FLOWMANAGER_DATASOURCE_HOSTNAME="http://couchdb"
+FLOWMANAGER_MATCHMAKER_URL="http://localhost:9081"
 
 # Remove old containers (exit code is ignored)
 docker rm -f couchdb 2>/dev/null || true
@@ -48,13 +43,15 @@ docker rmi -f $UNIVERSAL_IMAGE 2>/dev/null || true
 # Build image
 docker build -t $UNIVERSAL_IMAGE .
 
-# Start containers and load preferences data
+# Start the CouchDB container
 docker run -d -p $COUCHDB_PORT:$COUCHDB_PORT --name couchdb $COUCHDB_IMAGE
 
 # Wait for CouchDB
 sleep $COUCHDB_HEALTHCHECK_DELAY
 curl --retry $COUCHDB_HEALTHCHECK_TIMEOUT --retry-delay 1 --retry-connrefused http://localhost:$COUCHDB_PORT
 
-# docker run -d -p $PREFERENCES_PORT:$PREFERENCES_PORT --name preferences --link couchdb -e NODE_ENV=$PREFERENCES_CONFIG -e GPII_PREFERENCES_DATASOURCE_URL=$PREFERENCES_DATASOURCE_URL -e GPII_PREFERENCES_LISTEN_PORT=$PREFERENCES_PORT $UNIVERSAL_IMAGE
-docker run -d -p $FLOWMANAGER_PORT:$FLOWMANAGER_PORT --name flowmanager --link couchdb -e NODE_ENV=$FLOWMANAGER_CONFIG -e GPII_FLOWMANAGER_LISTEN_PORT=$FLOWMANAGER_PORT $UNIVERSAL_IMAGE
+# Start the flow manager container
+docker run -d -p $FLOWMANAGER_PORT:$FLOWMANAGER_PORT --name flowmanager --link couchdb -e NODE_ENV=$FLOWMANAGER_CONFIG -e GPII_FLOWMANAGER_LISTEN_PORT=$FLOWMANAGER_PORT -e FLOWMANAGER_DATASOURCE_HOSTNAME=$FLOWMANAGER_DATASOURCE_HOSTNAME -e FLOWMANAGER_DATASOURCE_PORT=$COUCHDB_PORT -e FLOWMANAGER_MATCHMAKER_URL=$FLOWMANAGER_MATCHMAKER_URL $UNIVERSAL_IMAGE
+
+# Load the CouchDB data
 docker run --rm --link couchdb -v $DBDATA_DIR:/data -e DBDATA_DIR=/data -e COUCHDB_URL=$DATALOADER_COUCHDB_URL -e CLEAR_INDEX=$DATALOADER_CLEAR_INDEX $DATALOADER_IMAGE
