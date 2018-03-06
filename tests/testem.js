6e58f728a029486275be64efa101426f630694c0
Tony Atkins <tony@raisingthefloor.org>
2018-03-06 12:51:21 +0100

    GPII-2486:  Refactored to use newer gpii-testem and built-in instrumenter, to ensure that browser tests can be run independently.

--- tests/testem.js
+++ tests/testem.js
@@ -24,16 +24,38 @@ gpii.tests.universal.testem.getTestemOptions = function (that) {
 };
 
 fluid.defaults("gpii.tests.universal.testem", {
-    gradeNames: ["gpii.testem.coverage"],
+    gradeNames: ["gpii.testem.instrumentation"],
     testPages:  ["tests/web/html/all-tests.html"],
     coverageDir: "coverage",
     reportsDir: "reports",
+    instrumentationOptions: {
+        "excludes": [],
+        "sources": ["./**/*.js"],
+        "nonSources": [
+            "./*.json",
+            "./**/*.json",
+            "./*.html",
+            "./**/*.html",
+            "./scripts/*.js",
+            "./testData/**/*.js",
+            "./tests/**",
+            "./!(node_modules)/**/*.!(js)",
+            "./!(node_modules)/*.!(js)",
+            "./!(node_modules)/tests/**/*.js",
+            "./!(node_modules)/**/tests/**/*.js",
+            "./!(node_modules)/test/**/*.js",
+            "./!(node_modules)/**/test/**/*.js",
+            "./!(node_modules)/**/webTests/**/*.js",
+            "./!(node_modules)/**/public/lib/**/*"
+        ]
+    },
     sourceDirs: {
         gpii: "%gpii-universal/gpii"
     },
     contentDirs: {
         browserify:   "%gpii-universal/browserify",
-        node_modules: "%gpii-universal/node_modules"
+        node_modules: "%gpii-universal/node_modules",
+        testData:     "%gpii-universal/testData"
     },
     invokers: {
         "getTestemOptions": {
@@ -44,10 +66,7 @@ fluid.defaults("gpii.tests.universal.testem", {
     testemOptions: {
         tap_quiet_logs: true,
         disable_watching: true,
-        skip: "PhantomJS,Opera,Safari",
-        "routes": {
-            "/gpii": "instrumented/gpii"
-        }
+        skip: "PhantomJS,Opera,Safari"
     }
 });
 
