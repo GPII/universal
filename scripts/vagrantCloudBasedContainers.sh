94e4e9bffbf7fef93c5d007bc6c1cdb89dbf1712
Cindy Qi Li <cli@ocadu.ca>
2018-05-04 13:58:03 -0400

    GPII-2630: Modified scripts/vagrantCloudBasedContainers.sh to use the new gpii-dataloader docker image.

--- scripts/vagrantCloudBasedContainers.sh
+++ scripts/vagrantCloudBasedContainers.sh
@@ -33,8 +33,8 @@ COUCHDB_PORT=5984
 COUCHDB_HEALTHCHECK_DELAY=2
 COUCHDB_HEALTHCHECK_TIMEOUT=30
 
-DBDATA_DIR_STATIC="/home/vagrant/sync/universal/testData/dbData"
-DBDATA_DIR_BUILD="/home/vagrant/sync/universal/build/dbData"
+STATIC_DATA_DIR="/home/vagrant/sync/universal/testData/dbData"
+BUILD_DATA_DIR="/home/vagrant/sync/universal/build/dbData"
 
 DATALOADER_IMAGE="cindyqili/gpii-dataloader"
 DATALOADER_COUCHDB_URL="http://couchdb:${COUCHDB_PORT}/gpii"
@@ -77,8 +77,7 @@ docker run -d -p $COUCHDB_PORT:$COUCHDB_PORT --name couchdb $COUCHDB_IMAGE
 wget -O /dev/null --retry-connrefused --waitretry=$COUCHDB_HEALTHCHECK_DELAY --read-timeout=20 --timeout=1 --tries=$COUCHDB_HEALTHCHECK_TIMEOUT http://localhost:$COUCHDB_PORT
 
 # Load the CouchDB data
-docker run --rm --link couchdb -v $DBDATA_DIR_STATIC:/data -e DBDATA_DIR=/data -e COUCHDB_URL=$DATALOADER_COUCHDB_URL -e CLEAR_INDEX=1 $DATALOADER_IMAGE
-docker run --rm --link couchdb -v $DBDATA_DIR_BUILD:/data -e DBDATA_DIR=/data -e COUCHDB_URL=$DATALOADER_COUCHDB_URL $DATALOADER_IMAGE
+docker run --rm --link couchdb -v $STATIC_DATA_DIR:/static_data -e STATIC_DATA_DIR=/static_data -v $BUILD_DATA_DIR:/build_data -e BUILD_DATA_DIR=/build_data -e COUCHDB_URL=$DATALOADER_COUCHDB_URL -e CLEAR_INDEX=1 $DATALOADER_IMAGE
 
 # Wait for the CouchDB views become accessible. Accessing the view URL forced the view index to build which take time.
 # The URL returns 500 when the index is not ready, so use "--retry-on-http-error" option to continue retries at 500 response code.
