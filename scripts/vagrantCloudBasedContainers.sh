b5dd8c05063311bd35ce3bda3ff062f66ff21e5d
Joseph Scheuhammer <clown@alum.mit.edu>
2018-07-06 16:56:55 -0400

    GPII-3138:  Update snapsets in the data base
    Fixed some (grievous) typos.

--- scripts/vagrantCloudBasedContainers.sh
+++ scripts/vagrantCloudBasedContainers.sh
@@ -33,9 +33,9 @@ COUCHDB_PORT=5984
 COUCHDB_HEALTHCHECK_DELAY=2
 COUCHDB_HEALTHCHECK_TIMEOUT=30
 if [ "$NO_REBUILD" == "true" ] ; then
-    CLEAR_INDEX=1
-else
     CLEAR_INDEX=
+else
+    CLEAR_INDEX=1
 fi
 
 UNIVERSAL_DIR="/home/vagrant/sync/universal"
@@ -83,7 +83,7 @@ docker run -d -p $COUCHDB_PORT:$COUCHDB_PORT --name couchdb $COUCHDB_IMAGE
 wget -O /dev/null --retry-connrefused --waitretry=$COUCHDB_HEALTHCHECK_DELAY --read-timeout=20 --timeout=1 --tries=$COUCHDB_HEALTHCHECK_TIMEOUT http://localhost:$COUCHDB_PORT
 
 # Load the CouchDB data
-docker run --rm --link couchdb -v $STATIC_DATA_DIR:/static_data -e STATIC_DATA_DIR=/static_data -v $BUILD_DATA_DIR:/build_data -e BUILD_DATA_DIR=/build_data -e COUCHDB_URL=$DATALOADER_COUCHDB_URL -e CLEAR_INDEX=$CLEAD_INDEX -e NODE_PATH=$UNIVERSAL_DIR $DATALOADER_IMAGE
+docker run --rm --link couchdb -v $STATIC_DATA_DIR:/static_data -e STATIC_DATA_DIR=/static_data -v $BUILD_DATA_DIR:/build_data -e BUILD_DATA_DIR=/build_data -e COUCHDB_URL=$DATALOADER_COUCHDB_URL -e CLEAR_INDEX=$CLEAR_INDEX -e NODE_PATH=$UNIVERSAL_DIR $DATALOADER_IMAGE
 
 # Wait for the CouchDB views become accessible. Accessing the view URL forced the view index to build which take time.
 # The URL returns 500 when the index is not ready, so use "--retry-on-http-error" option to continue retries at 500 response code.
