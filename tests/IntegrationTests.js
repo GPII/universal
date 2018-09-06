201c23e5e6712b4f8af4a3fee4bebd6a4b676321
Tony Atkins <tony@raisingthefloor.org>
2018-09-06 12:12:36 +0200

    GPII-2225: Uncommented out linux and android tests.

--- tests/IntegrationTests.js
+++ tests/IntegrationTests.js
@@ -29,6 +29,5 @@ fluid.require("%gpii-universal");
 gpii.loadTestingSupport();
 
 gpii.test.runSuitesWithFiltering(require("./platform/index-windows.js"), __dirname, ["gpii.test.integration.testCaseHolder.windows"]);
-// TODO: uncomment these
-//gpii.test.runSuitesWithFiltering(require("./platform/index-linux.js"), __dirname, ["gpii.test.integration.testCaseHolder.linux"]);
-//gpii.test.runSuitesWithFiltering(require("./platform/index-android.js"), __dirname, ["gpii.test.integration.testCaseHolder.android"]);
+gpii.test.runSuitesWithFiltering(require("./platform/index-linux.js"), __dirname, ["gpii.test.integration.testCaseHolder.linux"]);
+gpii.test.runSuitesWithFiltering(require("./platform/index-android.js"), __dirname, ["gpii.test.integration.testCaseHolder.android"]);
