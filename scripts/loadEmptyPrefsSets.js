b2950930970c9e1ede7645b8f327de70d0472ef0
Javier Hernández <javi@raisingthefloor.org>
2018-09-25 17:57:22 +0200

    GPII-3381: Save the created uuids into a file
    Added two optional parameters and made some clean up.

--- scripts/loadEmptyPrefsSets.js
+++ scripts/loadEmptyPrefsSets.js
@@ -18,11 +18,20 @@ https://github.com/GPII/universal/blob/master/LICENSE.txt
 //   you need to provide the credentials to CouchDB, you need to use the form
 //   "http://user:pass@host:port/dbname".
 //   * NEW_USERS: The amount of new empty preferences sets you want to create
+// Optional parameters
+//   * OUTPUT_FILENAME: The file to save the list of users created. By default,
+//   it will generate a file called "generated-prefsSets-<TIMESTAMP>", just provide a
+//   different value if you want it to be a different file.
+//   * SHOW_PROGRESS: Whether you want to see the users as they are created. Default is
+//   0 (no progress), set it to 1 if you want it to be the other way round.
+//
+// See this JIRA for more information: https://issues.gpii.net/browse/GPII-3381
 //
 
 "use strict";
 
 var process = require("process"),
+    fs = require("fs"),
     fluid = require("infusion"),
     gpii = fluid.registerNamespace("gpii"),
     uuid = uuid || require("node-uuid");
@@ -30,11 +39,10 @@ var process = require("process"),
 require("./shared/prefsSetsDbUtils.js");
 require("gpii-pouchdb");
 
-
 fluid.defaults("gpii.uuidLoader", {
     gradeNames: ["gpii.pouch"],
+    outputFileName: process.env.OUTPUT_FILE || "generated-prefSets-" + new Date().toISOString(),
     dbOptions: {
-        //name: "http://localhost:5984/gpii"
         name: process.env.COUCHDB_URL
     },
     invokers: {
@@ -51,7 +59,9 @@ fluid.defaults("gpii.uuidLoader", {
 
 gpii.uuidLoader.dbPut = function (that, doc) {
     var promise = fluid.promise();
-    // We only perform the put after checking that the uuid doesn't exist in DB
+    // We only perform the put after checking that the uuid doesn't exist in DB,
+    // just in case somebody wants to reuse this in the future.
+    //
     that.get(doc._id).then(function (/* record */) {
         promise.reject({isError: true, message: "docId already exists in DB"});
     }, function (err) {
@@ -85,22 +95,33 @@ gpii.uuidLoader.addPrefsSet = function (that, gpiiKey, prefSafe) {
 function createNewUsers(uuidLoader) {
     var data = gpii.prefsSetsDbUtils.generatePrefsSet(uuid.v4(), gpii.prefsSetsDbUtils.emptyPrefsSetBlock);
     uuidLoader.addPrefsSet(data.key, data.prefsSafe).then(function (newUser) {
-        console.log("## New user created:", newUser);
+        fs.appendFileSync(uuidLoader.options.outputFileName, newUser + "\n");
+        if (process.env.SHOW_PROGRESS) {
+            console.log("User",
+                        process.env.NEW_USERS - n + 1, "of",
+                        process.env.NEW_USERS, "created:",
+                        newUser);
+        }
+
         if (n > 1) {
             n--;
             createNewUsers(uuidLoader);
         } else {
-            console.log("## We're done adding new users! :)");
+            console.log("We're done adding new users! :)");
+            console.log("Remember, you have a list of the created users here:", uuidLoader.options.outputFileName);
         }
     }, function (error) {
-        console.log("## Got an error, let's stop it here. :/", error);
+        console.log("Got an error, let's stop it here. :/", error);
+        console.log("You can find the users we could create before crashing here:", uuidLoader.options.outputFileName);
     });
 };
 
 // This triggers the "real action"
 //
 var n = parseInt(process.env.NEW_USERS);
+
 if (n > 0) {
     var uuidLoader = gpii.uuidLoader();
+    console.log("Saving the list of generated users into:", uuidLoader.options.outputFileName);
     createNewUsers(uuidLoader);
 }
