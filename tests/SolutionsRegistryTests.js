e30ada088297d442fcde00a3a9c10c38ba30b8df
Tony Atkins <tony@raisingthefloor.org>
2019-01-16 14:45:12 +0100

    GPII-2839: Added initial SR validation checks and fixed errors revealed.

--- tests/SolutionsRegistryTests.js
+++ tests/SolutionsRegistryTests.js
@@ -12,11 +12,14 @@ require("gpii-json-schema");
 
 var solutionSchema = require("./data/schemas/solution-schema.json5");
 
-jqUnit.module("Sanity checks for all Solutions Registry data.");
+jqUnit.module("Sanity checks for Solutions Registry data.");
 
 fluid.registerNamespace("gpii.tests.universal.solutionsRegistry");
 
 gpii.tests.universal.solutionsRegistry.checkAllSolutions = function (that) {
+    var resolvedGenericTermsPath = fluid.module.resolvePath(that.options.genericPreferenceTermsPath);
+    var genericTerms = require(resolvedGenericTermsPath);
+
     var resolvedSrPath = fluid.module.resolvePath(that.options.solutionsRegistryPath);
     var files = fs.readdirSync(resolvedSrPath);
     fluid.each(files, function (filename) {
@@ -26,34 +29,71 @@ gpii.tests.universal.solutionsRegistry.checkAllSolutions = function (that) {
             var filePath = path.resolve(resolvedSrPath, filename);
             var singleFileSolutions = require(filePath);
             fluid.each(singleFileSolutions, function (solutionDef, solutionKey) {
-                gpii.tests.universal.solutionsRegistry.checkSingleSolution(filename, solutionKey, solutionDef);
+                gpii.tests.universal.solutionsRegistry.checkSingleSolution(filename, solutionKey, solutionDef, genericTerms);
             });
         }
+    });
+
+    gpii.tests.universal.solutionsRegistry.validateGenericPreferenceSchemas(genericTerms);
+};
+
+// Validate the generic preference settings to confirm that they're at least valid GSS schemas.
+gpii.tests.universal.solutionsRegistry.validateGenericPreferenceSchemas = function (genericTerms) {
+    jqUnit.test("Validating generic preference terms.", function () {
+        fluid.each(genericTerms, function (termDef, termKey) {
+            if (termDef.schema) {
+                var validationResults = gpii.schema.validator.validateSchema(termDef.schema);
+
+                // We perform these checks a bit flipped so that only a particular value trips it, and not a missing value.
+                jqUnit.assertFalse("There should be no validation errors in generic preference term '" + termKey + "'.", validationResults.isValid === false);
+                jqUnit.assertFalse("There should be no low-level errors in generic preference term '" + termKey + "'.", validationResults.isError === true);
+            }
+            else {
+                jqUnit.fail("Generic Preference Term '" + termKey + "' has no schema.");
+            }
+        });
     });
 };
 
-gpii.tests.universal.solutionsRegistry.checkSingleSolution = function (filename, solutionKey, solutionDef) {
-    jqUnit.test("Checking validity of solution '" + solutionKey + "' in file '" + filename + "'.", function () {
-        var validationResults = gpii.schema.validator.validate(solutionDef, solutionSchema);
-        jqUnit.assertFalse("There should be no low-level errors attempting to validate the solution entry.", validationResults.isError === true);
-        jqUnit.assertTrue("There should be no validation errors in the solution definition.", validationResults.isValid);
-        if (!validationResults.isError && !validationResults.isValid) {
-            var localisedErrors = gpii.schema.validator.localiseErrors(validationResults.errors, solutionDef);
+gpii.tests.universal.solutionsRegistry.checkSingleSolution = function (filename, solutionKey, solutionDef, genericTerms) {
+    jqUnit.test("Sanity-checking solution '" + solutionKey + "' in file '" + filename + "'.", function () {
+        gpii.tests.universal.solutionsRegistry.validateSolution(filename, solutionKey, solutionDef);
 
-            fluid.each(localisedErrors, function (singleError) {
-                fluid.log("Validation error in solution '" + solutionKey + "' in file '" + filename + "':");
-                fluid.log("  - " + singleError.dataPath.join(".") + ": " + singleError.message);
-            });
-        }
+        gpii.tests.universal.solutionsRegistry.checkCapabilities(filename, solutionKey, solutionDef, genericTerms);
+
+        // TODO: Ensure that all forward and inverse transforms are sane, i.e. that they can at least be used with an empty object.
+
+        // TODO: Ensure that all references to generic preference settings in transforms actually refer to generic preference settings that exist.
+
+        gpii.tests.universal.solutionsRegistry.checkDefaults(filename, solutionKey, solutionDef);
     });
 
-    // TODO: Make a schema for and validate the generic preference settings.
+};
 
-    // TODO: If there are default values, test them against the schema.
+gpii.tests.universal.solutionsRegistry.validateSolution = function (filename, solutionKey, solutionDef) {
+    var validationResults = gpii.schema.validator.validate(solutionDef, solutionSchema);
+    jqUnit.assertFalse("There should be no low-level errors attempting to validate the solution entry.", validationResults.isError === true);
+    jqUnit.assertTrue("There should be no validation errors in the solution definition.", validationResults.isValid);
+    if (!validationResults.isError && !validationResults.isValid) {
+        var localisedErrors = gpii.schema.validator.localiseErrors(validationResults.errors, solutionDef);
 
-    // TODO: Ensuring that all forward and inverse transforms are sane, i.e. that they can at least be used with an empty object.
+        fluid.each(localisedErrors, function (singleError) {
+            fluid.log("Validation error in solution '" + solutionKey + "' in file '" + filename + "':");
+            fluid.log("  - " + singleError.dataPath.join(".") + ": " + singleError.message);
+        });
+    }
+};
 
-    // TODO: Ensuring that all references to generic preference settings actually refer to generic preference settings that exist.
+// Ensure that all "capabilities" refer to a generic preference setting.
+gpii.tests.universal.solutionsRegistry.checkCapabilities = function (filename, solutionKey, solutionDef, genericTerms) {
+    if (solutionDef.capabilities && solutionDef.capabilities.length) {
+        fluid.each(solutionDef.capabilities, function (capabilityKey) {
+            jqUnit.assertTrue("The capability '" + capabilityKey + "' specified in solution '" + solutionKey + "' should exist.", genericTerms[capabilityKey] !== undefined);
+        });
+    }
+    else {
+        jqUnit.assert("Entry '" + solutionKey + "' has no capabilities, which is fine.");
+    }
 };
 
 fluid.defaults("gpii.tests.universal.solutionsRegistry", {
@@ -66,5 +106,20 @@ fluid.defaults("gpii.tests.universal.solutionsRegistry", {
         }
     }
 });
+
+// If there are default values, test them against the schema.
+gpii.tests.universal.solutionsRegistry.checkDefaults = function (filename, solutionKey, solutionDef) {
+    fluid.each(solutionDef.settingsHandlers, function (settingsHandler) {
+        fluid.each(settingsHandler.supportedSettings, function (supportedSetting, settingKey) {
+            // This only works for simple settings, and not for deeply nested objects.
+            var defaultValue = fluid.get(supportedSetting, "schema.default");
+            if (defaultValue !== undefined) {
+                var validationResults = gpii.schema.validator.validate(defaultValue, supportedSetting.schema);
+                jqUnit.assertFalse("There should be no low-level errors when validating the default value for setting '" + settingKey + "' in solution '" + solutionKey + "' should be valid according to its own schema.", validationResults.isError === true);
+                jqUnit.assertTrue("The default for setting '" + settingKey + "' in solution '" + solutionKey + "' should be valid according to its own schema.", validationResults.isValid);
+            }
+        });
+    });
+};
 
 gpii.tests.universal.solutionsRegistry();
