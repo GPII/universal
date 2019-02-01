25c03fc38832d1f859f8de3e82c0a7334853a72c
Tony Atkins <tony@raisingthefloor.org>
2019-02-01 16:20:55 +0100

    GPII-3619: Fixed nested array in test sequence.

--- tests/shared/PSPIntegrationTestDefs.js
+++ tests/shared/PSPIntegrationTestDefs.js
@@ -733,43 +733,50 @@ gpii.tests.pspIntegration.testDefs = [
         name: "GPII-3693: \"reset all\" does not corrupt PSPChannel's model",
         expect: 9,
         sequence: [
-            [
-                {
-                    func: "{pspClient}.connect"
-                }, {
-                    event: "{pspClient}.events.onConnect",
-                    listener: "gpii.tests.pspIntegration.connectionSucceeded"
-                }, {
-                    event: "{pspClient}.events.onReceiveMessage",
-                    listener: "gpii.tests.pspIntegration.checkPayload",
-                    args: ["{arguments}.0", "modelChanged"]
-                }, {
-                    funcName: "gpii.tests.pspIntegration.sendMsg",
-                    args: [ "{pspClient}", ["preferences", "http://registry\\.gpii\\.net/common/magnification"], 3]
-                }, {
-                    event: "{pspClient}.events.onReceiveMessage",
-                    listener: "gpii.tests.pspIntegration.checkPayload",
-                    args: ["{arguments}.0", "modelChanged", ["http://registry\\.gpii\\.net/common/magnification"]]
-                }, {
-                    func: "{resetRequest}.send"
-                }, {
-                    event: "{resetRequest}.events.onComplete",
-                    listener: "gpii.tests.pspIntegration.checkResetResponse",
-                    args: ["{arguments}.0"]
-                }, {
-                    // When "noUser" keys back in, PSP client receives empty settingControls block.
-                    event: "{pspClient}.events.onReceiveMessage",
-                    listener: "gpii.tests.pspIntegration.checkPayload",
-                    args: ["{arguments}.0", "modelChanged", []]
-                }, {
-                    funcName: "gpii.tests.pspIntegration.sendMsg",
-                    args: [ "{pspClient}", ["preferences", "http://registry\\.gpii\\.net/common/cursorSize"], 0.9]
-                }, {
-                    event: "{pspClient}.events.onReceiveMessage",
-                    listener: "gpii.tests.pspIntegration.checkPayload",
-                    args: ["{arguments}.0", "modelChanged", ["http://registry\\.gpii\\.net/common/cursorSize"]]
-                }
-            ]
+            {
+                func: "{pspClient}.connect"
+            },
+            {
+                event: "{pspClient}.events.onConnect",
+                listener: "gpii.tests.pspIntegration.connectionSucceeded"
+            },
+            {
+                event: "{pspClient}.events.onReceiveMessage",
+                listener: "gpii.tests.pspIntegration.checkPayload",
+                args: ["{arguments}.0", "modelChanged"]
+            },
+            {
+                funcName: "gpii.tests.pspIntegration.sendMsg",
+                args: [ "{pspClient}", ["preferences", "http://registry\\.gpii\\.net/common/magnification"], 3]
+            },
+            {
+                event: "{pspClient}.events.onReceiveMessage",
+                listener: "gpii.tests.pspIntegration.checkPayload",
+                args: ["{arguments}.0", "modelChanged", ["http://registry\\.gpii\\.net/common/magnification"]]
+            },
+            {
+                func: "{resetRequest}.send"
+            },
+            {
+                event: "{resetRequest}.events.onComplete",
+                listener: "gpii.tests.pspIntegration.checkResetResponse",
+                args: ["{arguments}.0"]
+            },
+            {
+                // When "noUser" keys back in, PSP client receives empty settingControls block.
+                event: "{pspClient}.events.onReceiveMessage",
+                listener: "gpii.tests.pspIntegration.checkPayload",
+                args: ["{arguments}.0", "modelChanged", []]
+            },
+            {
+                funcName: "gpii.tests.pspIntegration.sendMsg",
+                args: [ "{pspClient}", ["preferences", "http://registry\\.gpii\\.net/common/cursorSize"], 0.9]
+            },
+            {
+                event: "{pspClient}.events.onReceiveMessage",
+                listener: "gpii.tests.pspIntegration.checkPayload",
+                args: ["{arguments}.0", "modelChanged", ["http://registry\\.gpii\\.net/common/cursorSize"]]
+            }
         ]
     }
 ];
