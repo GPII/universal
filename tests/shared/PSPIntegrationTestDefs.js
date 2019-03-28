9a9f4da6f6b96802f84c8663ca2aba474db27f05
Cindy Qi Li <cli@ocadu.ca>
2019-03-28 16:47:30 -0400

    GPII-3828: PSP channel reports default setting values from reset to std file in "schema.default" field rather than "value" field.

--- tests/shared/PSPIntegrationTestDefs.js
+++ tests/shared/PSPIntegrationTestDefs.js
@@ -54,7 +54,7 @@ gpii.tests.pspIntegration.sendContextChange = function (client, newContext) {
 gpii.tests.pspIntegration.checkPayload = function (data, expectedType, expectedSettingControls) {
     jqUnit.assertEquals("Checking message from PSP: ", expectedType, data.type);
     if (expectedSettingControls) {
-        var actualSettingControls = fluid.keys(data.payload.value.settingControls);
+        var actualSettingControls = data.payload.value.settingControls;
         jqUnit.assertDeepEq("Checking received settingControls from PSP: ", expectedSettingControls, actualSettingControls);
     }
 };
@@ -732,66 +732,35 @@ gpii.tests.pspIntegration.testDefs = [
     }, {
         name: "GPII-3693: \"reset all\" does not corrupt PSPChannel's model",
         expect: 10,
-        sequence: [
-            {
-                func: "{pspClient}.connect"
+        expectedSettingControls: {
+            noUser: {},
+            afterChangeMagnification: {
+                "http://registry\\.gpii\\.net/common/magnification": {
+                    "value": 3,
+                    "schema": {
+                        "title": "Magnification",
+                        "description": "Level of magnification",
+                        "type": "number",
+                        "default": 1,
+                        "minimum": 1,
+                        "multipleOf": 0.1
+                    },
+                    "liveness": "live"
+                }
             },
-            {
-                event: "{pspClient}.events.onConnect",
-                listener: "gpii.tests.pspIntegration.connectionSucceeded"
-            },
-            {
-                event: "{pspClient}.events.onReceiveMessage",
-                listener: "gpii.tests.pspIntegration.checkPayload",
-                args: ["{arguments}.0", "modelChanged", []]
-            },
-            {
-                funcName: "gpii.tests.pspIntegration.sendMsg",
-                args: [ "{pspClient}", ["preferences", "http://registry\\.gpii\\.net/common/magnification"], 3]
-            },
-            {
-                event: "{pspClient}.events.onReceiveMessage",
-                listener: "gpii.tests.pspIntegration.checkPayload",
-                args: ["{arguments}.0", "modelChanged", [
-                    "http://registry\\.gpii\\.net/common/magnification"
-                ]]
-            },
-            {
-                func: "{resetRequest}.send"
-            },
-            {
-                event: "{resetRequest}.events.onComplete",
-                listener: "gpii.tests.pspIntegration.checkResetResponse",
-                args: ["{arguments}.0"]
-            },
-            {
-                // When "noUser" keys back in, PSP client receives empty settingControls block.
-                event: "{pspClient}.events.onReceiveMessage",
-                listener: "gpii.tests.pspIntegration.checkPayload",
-                args: ["{arguments}.0", "modelChanged", []]
-            },
-            {
-                funcName: "gpii.tests.pspIntegration.sendMsg",
-                args: [ "{pspClient}", ["preferences", "http://registry\\.gpii\\.net/common/cursorSize"], 0.9]
-            },
-            {
-                event: "{pspClient}.events.onReceiveMessage",
-                listener: "gpii.tests.pspIntegration.checkPayload",
-                args: ["{arguments}.0", "modelChanged", [
-                    "http://registry\\.gpii\\.net/common/cursorSize"
-                ]]
-            }
-        ]
-    }, {
-        name: "GPII-3828: PSPChannel reports default settings from the reset to default file when settingControls block is empty",
-        expect: 10,
-        "defaultSettings": {
-            "contexts": {
-                "gpii-default": {
-                    "preferences": {
-                        "http://registry.gpii.net/common/cursorSize": 0.5,
-                        "http://registry.gpii.net/common/volume": 0.75
-                    }
+            afterChangeCursorSize: {
+                "http://registry\\.gpii\\.net/common/cursorSize": {
+                    "value": 0.9,
+                    "schema": {
+                        "title": "Cursor Size",
+                        "description": "Cursor size",
+                        "type": "number",
+                        "default": 0.5,
+                        "minimum": 0,
+                        "maximum": 1,
+                        "multipleOf": 0.1
+                    },
+                    "liveness": "live"
                 }
             }
         },
@@ -806,10 +775,7 @@ gpii.tests.pspIntegration.testDefs = [
             {
                 event: "{pspClient}.events.onReceiveMessage",
                 listener: "gpii.tests.pspIntegration.checkPayload",
-                args: ["{arguments}.0", "modelChanged", [
-                    "http://registry\\.gpii\\.net/common/cursorSize",
-                    "http://registry\\.gpii\\.net/common/volume"
-                ]]
+                args: ["{arguments}.0", "modelChanged", "{that}.options.expectedSettingControls.noUser"]
             },
             {
                 funcName: "gpii.tests.pspIntegration.sendMsg",
@@ -818,11 +784,7 @@ gpii.tests.pspIntegration.testDefs = [
             {
                 event: "{pspClient}.events.onReceiveMessage",
                 listener: "gpii.tests.pspIntegration.checkPayload",
-                args: ["{arguments}.0", "modelChanged", [
-                    "http://registry\\.gpii\\.net/common/cursorSize",
-                    "http://registry\\.gpii\\.net/common/volume",
-                    "http://registry\\.gpii\\.net/common/magnification"
-                ]]
+                args: ["{arguments}.0", "modelChanged", "{that}.options.expectedSettingControls.afterChangeMagnification"]
             },
             {
                 func: "{resetRequest}.send"
@@ -836,10 +798,7 @@ gpii.tests.pspIntegration.testDefs = [
                 // When "noUser" keys back in, PSP client receives empty settingControls block.
                 event: "{pspClient}.events.onReceiveMessage",
                 listener: "gpii.tests.pspIntegration.checkPayload",
-                args: ["{arguments}.0", "modelChanged", [
-                    "http://registry\\.gpii\\.net/common/cursorSize",
-                    "http://registry\\.gpii\\.net/common/volume"
-                ]]
+                args: ["{arguments}.0", "modelChanged", "{that}.options.expectedSettingControls.noUser"]
             },
             {
                 funcName: "gpii.tests.pspIntegration.sendMsg",
@@ -848,10 +807,158 @@ gpii.tests.pspIntegration.testDefs = [
             {
                 event: "{pspClient}.events.onReceiveMessage",
                 listener: "gpii.tests.pspIntegration.checkPayload",
-                args: ["{arguments}.0", "modelChanged", [
-                    "http://registry\\.gpii\\.net/common/cursorSize",
-                    "http://registry\\.gpii\\.net/common/volume"
-                ]]
+                args: ["{arguments}.0", "modelChanged", "{that}.options.expectedSettingControls.afterChangeCursorSize"]
+            }
+        ]
+    }, {
+        name: "GPII-3828: PSPChannel reports default settings from the reset to default file when settingControls block is empty",
+        expect: 10,
+        "defaultSettings": {
+            "contexts": {
+                "gpii-default": {
+                    "preferences": {
+                        "http://registry.gpii.net/common/cursorSize": 0.8,
+                        "http://registry.gpii.net/common/volume": 1
+                    }
+                }
+            }
+        },
+        expectedSettingControls: {
+            noUser: {
+                "http://registry\\.gpii\\.net/common/cursorSize": {
+                    "schema": {
+                        "title": "Cursor Size",
+                        "description": "Cursor size",
+                        "type": "number",
+                        "default": 0.8,
+                        "minimum": 0,
+                        "maximum": 1,
+                        "multipleOf": 0.1
+                    },
+                    "liveness": "live"
+                },
+                "http://registry\\.gpii\\.net/common/volume": {
+                    "schema": {
+                        "title": "Volume",
+                        "description": "General volume of the operating system",
+                        "type": "number",
+                        "minimum": 0,
+                        "maximum": 1,
+                        "default": 1
+                    },
+                    "liveness": "live"
+                }
+            },
+            afterChangeMagnification: {
+                "http://registry\\.gpii\\.net/common/cursorSize": {
+                    "schema": {
+                        "title": "Cursor Size",
+                        "description": "Cursor size",
+                        "type": "number",
+                        "default": 0.8,
+                        "minimum": 0,
+                        "maximum": 1,
+                        "multipleOf": 0.1
+                    },
+                    "liveness": "live"
+                },
+                "http://registry\\.gpii\\.net/common/volume": {
+                    "schema": {
+                        "title": "Volume",
+                        "description": "General volume of the operating system",
+                        "type": "number",
+                        "minimum": 0,
+                        "maximum": 1,
+                        "default": 1
+                    },
+                    "liveness": "live"
+                },
+                "http://registry\\.gpii\\.net/common/magnification": {
+                    "value": 3,
+                    "schema": {
+                        "title": "Magnification",
+                        "description": "Level of magnification",
+                        "type": "number",
+                        "default": 1,
+                        "minimum": 1,
+                        "multipleOf": 0.1
+                    },
+                    "liveness": "live"
+                }
+            },
+            afterChangeCursorSize: {
+                "http://registry\\.gpii\\.net/common/cursorSize": {
+                    "value": 0.9,
+                    "schema": {
+                        "title": "Cursor Size",
+                        "description": "Cursor size",
+                        "type": "number",
+                        "default": 0.8,
+                        "minimum": 0,
+                        "maximum": 1,
+                        "multipleOf": 0.1
+                    },
+                    "liveness": "live"
+                },
+                "http://registry\\.gpii\\.net/common/volume": {
+                    "schema": {
+                        "title": "Volume",
+                        "description": "General volume of the operating system",
+                        "type": "number",
+                        "minimum": 0,
+                        "maximum": 1,
+                        "default": 1
+                    },
+                    "liveness": "live"
+                }
+            }
+        },
+        sequence: [
+            {
+                func: "{pspClient}.connect"
+            },
+            {
+                event: "{pspClient}.events.onConnect",
+                listener: "gpii.tests.pspIntegration.connectionSucceeded"
+            },
+            {
+                event: "{pspClient}.events.onReceiveMessage",
+                listener: "gpii.tests.pspIntegration.checkPayload",
+                args: ["{arguments}.0", "modelChanged", "{that}.options.expectedSettingControls.noUser"]
+            },
+            {
+                funcName: "gpii.tests.pspIntegration.sendMsg",
+                args: [ "{pspClient}", ["preferences", "http://registry\\.gpii\\.net/common/magnification"], 3]
+            },
+            {
+                event: "{pspClient}.events.onReceiveMessage",
+                listener: "gpii.tests.pspIntegration.checkPayload",
+                args: ["{arguments}.0", "modelChanged", "{that}.options.expectedSettingControls.afterChangeMagnification"]
+            },
+            {
+                func: "{resetRequest}.send"
+            },
+            {
+                event: "{resetRequest}.events.onComplete",
+                listener: "gpii.tests.pspIntegration.checkResetResponse",
+                args: ["{arguments}.0"]
+            },
+            {
+                // When "noUser" keys back in, PSP client receives empty settingControls block.
+                event: "{pspClient}.events.onReceiveMessage",
+                listener: "gpii.tests.pspIntegration.checkPayload",
+                args: ["{arguments}.0", "modelChanged", "{that}.options.expectedSettingControls.noUser"]
+            },
+            {
+                // change a setting that is in default settings from the reset to standard file.
+                // settingControls.settingKey.schema.default should be set to the value from the default setting.
+                funcName: "gpii.tests.pspIntegration.sendMsg",
+                args: [ "{pspClient}", ["preferences", "http://registry\\.gpii\\.net/common/cursorSize"], 0.9]
+            },
+            {
+                event: "{pspClient}.events.onReceiveMessage",
+                listener: "gpii.tests.pspIntegration.checkPayload",
+                args: ["{arguments}.0", "modelChanged", "{that}.options.expectedSettingControls.afterChangeCursorSize"]
             }
         ]
     }
