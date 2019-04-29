fe0d0638283d17bb349774d6199f86bfd8da302b
Joseph Scheuhammer <clown@alum.mit.edu>
2019-04-29 16:57:42 -0400

    GPII-3646:  Modified dockerDeleteExpiredAccessTokens.sh shell script
    Modifed the script to not call "docker run ..." within it, but to
    expect to be run from within a docker container.

--- scripts/dockerDeleteExpiredAccessTokens.sh
+++ scripts/dockerDeleteExpiredAccessTokens.sh
@@ -12,25 +12,19 @@
 # tokens (GPII App Installation Authorization records) from CouchDB using a
 # docker image of gpii-universal
 
-GPII_UNIVERSAL_IMAGE=${GPII_UNIVERSAL_IMAGE:-"vagrant-universal"}
 GPII_APP_DIR=${GPII_APP_DIR:-"/app"}
-GPII_COUCHDB_CONTAINER=${GPII_COUCHDB_CONTAINER:-"couchdb"}
 GPII_COUCHDB_URL=${GPII_COUCHDB_URL:-"http://couchdb:5984/gpii"}
 DELETE_ALL_FLAG=${DELETE_ALL_FLAG:-""}
 
 GPII_COUCHDB_URL_SANITIZED=$(echo "${GPII_COUCHDB_URL}" | sed -e 's,\(://\)[^/]*\(@\),\1<SENSITIVE>\2,g')
-
-DELETE_ACCESS_TOKENS_CMD="node ${GPII_APP_DIR}/scripts/deleteExpiredAccessTokens.js ${GPII_COUCHDB_URL} ${DELETE_ALL_FLAG}"
 
 log() {
   echo "$(date +'%Y-%m-%d %H:%M:%S') - $1"
 }
 
 log "Delete expired access tokens: starting..."
-log "Universal docker image: ${GPII_UNIVERSAL_IMAGE}"
-log "CouchDB docker container: ${GPII_COUCHDB_CONTAINER}"
 log "CouchDB: ${GPII_COUCHDB_URL_SANITIZED}"
 log "Delete all flag: ${DELETE_ALL_FLAG}"
 
-docker run --name deleteExpiredAccessTokens --rm --link $GPII_COUCHDB_CONTAINER $GPII_UNIVERSAL_IMAGE $DELETE_ACCESS_TOKENS_CMD
+node ${GPII_APP_DIR}/scripts/deleteExpiredAccessTokens.js ${GPII_COUCHDB_URL} ${DELETE_ALL_FLAG}
 
