8864c44ca2203db9b754979cab51ac598407b521
Joseph Scheuhammer <clown@alum.mit.edu>
2019-05-09 13:44:38 -0400

    GPII-3646:  Added wait for CouchDB to be ready

--- scripts/dockerDeleteExpiredAccessTokens.sh
+++ scripts/dockerDeleteExpiredAccessTokens.sh
@@ -17,6 +17,10 @@ GPII_COUCHDB_URL=${GPII_COUCHDB_URL:-"http://couchdb:5984/gpii"}
 DELETE_ALL=${DELETE_ALL:-""}
 
 GPII_COUCHDB_URL_SANITIZED=$(echo "${GPII_COUCHDB_URL}" | sed -e 's,\(://\)[^/]*\(@\),\1<SENSITIVE>\2,g')
+GPII_COUCHDB_URL_ROOT=$(echo "${GPII_COUCHDB_URL}" | sed 's/[^\/]*$//g')
+COUCHDB_HEALTHCHECK_DELAY=2
+COUCHDB_HEALTHCHECK_RETRIES=30
+
 DELETE_ACCESS_TOKENS_JS="${GPII_APP_DIR}/scripts/deleteExpiredAccessTokens.js"
 
 log() {
@@ -27,9 +31,19 @@ log "Delete expired access tokens: starting..."
 log "CouchDB: ${GPII_COUCHDB_URL_SANITIZED}"
 log "Delete all flag: ${DELETE_ALL}"
 
+log "Waiting for couchdb to be ready"
+for ((i=0; i<$COUCHDB_HEALTHCHECK_RETRIES; i++));
+do
+    RET_CODE=$(curl --write-out '%{http_code}' --silent --output /dev/null "${GPII_COUCHDB_URL_ROOT}/_up")
+    if [ "$RET_CODE" = '200' ]; then
+        break
+    fi
+    sleep $COUCHDB_HEALTHCHECK_DELAY
+done
+
 node ${DELETE_ACCESS_TOKENS_JS} ${GPII_COUCHDB_URL} ${DELETE_ALL}
-err=$?
-if [ "${err}" != '0' ]; then
-  log "${DELETE_ACCESS_TOKENS_JS} failed with ${err}, exiting"
+RET_CODE=$?
+if [ "${RET_CODE}" != '0' ]; then
+  log "${DELETE_ACCESS_TOKENS_JS} failed with exit code ${RET_CODE}, exiting"
 fi
-exit "${err}"
+exit "${RET_CODE}"
