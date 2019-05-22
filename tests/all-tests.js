37264a5359fffe4242509ebd79152253814b6933
Cindy Qi Li <cli@ocadu.ca>
2019-05-22 15:58:00 -0400

    GPII-3747: Improvements as per code review.

--- tests/all-tests.js
+++ tests/all-tests.js
@@ -59,7 +59,7 @@ var testIncludes = [
     "./UserLogonRequestTests.js",
     "../gpii/node_modules/accessRequester/test/AccessRequesterTests.js",
     "../gpii/node_modules/contextManager/test/ContextManagerTests.js",
-    "../gpii/node_modules/deviceReporter/test/StaticDeviceReporter.js",
+    "../gpii/node_modules/deviceReporter/test/StaticDeviceReporterTests.js",
     "../gpii/node_modules/eventLog/test/all-tests.js",
     "../gpii/node_modules/flatMatchMaker/test/FlatMatchMakerTests.js",
     "../gpii/node_modules/flowManager/test/BrowserChannelTests.js",
