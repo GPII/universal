5223e460fff2b4a958914d3693b4f786c8fee622
Cindy Qi Li <cli@ocadu.ca>
2019-06-19 17:01:10 -0400

    GPII-3717: Fixed production config tests.

--- tests/production/ProductionTestsUtils.js
+++ tests/production/ProductionTestsUtils.js
@@ -94,64 +94,11 @@ fluid.defaults("gpii.test.disruption.settings.sequenceGrade", {
     gradeNames: ["gpii.test.disruption", "gpii.tests.productionConfigTesting.flushAccessTokens"]
 });
 
-gpii.tests.productionConfigTesting.getKeyOrPrefsFromFile = function (filePath, id) {
-    var dataArray = fluid.require(filePath);
-    return fluid.find(dataArray, function (anEntry) {
-        if (anEntry._id === id) {
-            return anEntry;
-        }
-    }, null);
-};
-
-gpii.tests.productionConfigTesting.gpiiKeyNoPrefsSafe =
-    gpii.tests.productionConfigTesting.getKeyOrPrefsFromFile(
-        "%gpii-universal/tests/data/dbData/gpiiKeys.json",
-        "gpii_key_no_prefs_safe"
-    );
-
-gpii.tests.productionConfigTesting.settingsUserKey =
-    gpii.tests.productionConfigTesting.getKeyOrPrefsFromFile(
-        "%gpii-universal/build/tests/dbData/gpiiKeys.json",
-        "os_gnome"
-    );
-
-gpii.tests.productionConfigTesting.settingsUserPrefsSafe =
-    gpii.tests.productionConfigTesting.getKeyOrPrefsFromFile(
-        "%gpii-universal/build/tests/dbData/prefsSafes.json",
-        "prefsSafe-os_gnome"
-    );
-
-gpii.tests.productionConfigTesting.prefsUpdate = {
-    "contexts": {
-        "gpii-default": {
-            "name": "Default preferences",
-            "preferences": {
-                "http://registry.gpii.net/common/setting1": 12
-            }
-        }
-    }
-};
-
 gpii.tests.productionConfigTesting.testStatusCode = function (data, request) {
     jqUnit.assertEquals(
         "Checking status of " + request.options.path,
         request.options.expectedStatusCode, request.nativeResponse.statusCode
     );
-};
-
-gpii.tests.productionConfigTesting.testAddedToDatabase = function (data, request) {
-    var expected = request.options.expectedStatusCodes;
-    var actual = request.nativeResponse.statusCode;
-    jqUnit.assertNotEquals(
-        "Adding record to database using " + request.options.path +
-        ", status: " + actual,
-        expected.indexOf(actual), -1
-    );
-    // Store the added record's id and rev
-    if (actual === 201 || actual === 200) {
-        request.options.result = JSON.parse(data);
-        fluid.log(request.options.result);
-    }
 };
 
 gpii.tests.productionConfigTesting.testResponse = function (data, request) {
@@ -170,37 +117,6 @@ gpii.tests.productionConfigTesting.testStatusCodes = function (request) {
         "]; actual status: " + actual,
         fluid.contains(expected, actual)
     );
-};
-
-gpii.tests.productionConfigTesting.testGetThenSaveDocForDeletion = function (data, request) {
-    gpii.tests.productionConfigTesting.testStatusCodes(request);
-    // Mark and store the to-be-deleted record
-    if (request.nativeResponse.statusCode === 201 || request.nativeResponse.statusCode === 200) {
-        request.options.docToRemove = JSON.parse(data);
-        request.options.docToRemove._deleted = true;
-        fluid.log("Will remove ", request.options.docToRemove.type, " ", request.options.docToRemove._id);
-    } else {
-        fluid.log("Nothing to remove at ", request.options.path);
-    }
-};
-
-gpii.tests.productionConfigTesting.testGetExtraAccessTokens = function (data, accessTokensRequest) {
-    gpii.tests.productionConfigTesting.testStatusCodes(accessTokensRequest);
-    var tokens = JSON.parse(data);
-    if (tokens.rows) {
-        fluid.each(tokens.rows, function (aRow) {
-            fluid.each(accessTokensRequest.options.extraGpiiKeys, function (extraGpiiKey) {
-                var aToken = aRow.value.authorization;
-                if (aToken.gpiiKey === extraGpiiKey && aToken.clientCredentialId === "clientCredential-1") {
-                    aToken._deleted = true;
-                    accessTokensRequest.options.tokensToRemove.push(aToken);
-                    fluid.log("Will remove ", aToken.type, " for ", aToken.gpiiKey);
-                }
-            });
-        });
-    } else {
-        fluid.log("No access tokens to remove");
-    }
 };
 
 gpii.tests.productionConfigTesting.testGetAccessTokensForDeletion = function (data, accessTokensRequest) {
