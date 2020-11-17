f2972b9deeae48847a4ed951d1ab703b4e7bafc5
Javier Hernández <javi@raisingthefloor.org>
2020-11-17 22:55:42 +0100

    GPII-4517: Disabled Volume setting in integration/acceptance tests

--- tests/platform/windows/windows-builtIn-testSpec.js
+++ tests/platform/windows/windows-builtIn-testSpec.js
@@ -98,17 +98,18 @@ gpii.tests.windows.builtIn = [
                         "options": {
                             "functionName": "DoubleClickHeight"
                         }
-                    },
-                    {
-                        "settings": {
-                            "Volume": {
-                                "value": 0.7
-                            }
-                        },
-                        "options": {
-                            "functionName": "Volume"
-                        }
                     }
+                    // TODO: Disabled due to GPII-4518
+                    //{
+                    //    "settings": {
+                    //        "Volume": {
+                    //            "value": 0.7
+                    //        }
+                    //    },
+                    //    "options": {
+                    //        "functionName": "Volume"
+                    //    }
+                    //}
                 ]
             },
             "gpii.windows.spiSettingsHandler": {
